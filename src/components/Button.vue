f02ade7a0ec7645a224e8771ac27d3df9e8613f7
kamaljohnson <kamaljohnson12345@gmail.com>
2022-02-07 16:03:30 +0530

    refactor: button-full-width -> full-width

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -68,7 +68,7 @@ export default {
       type: String,
       default: null,
     },
-    buttonFullWidth: {
+    fullWidth: {
       type: Boolean,
       default: false
     },
@@ -89,7 +89,7 @@ export default {
           'bg-white text-gray-900 border hover:bg-gray-50 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400':
             this.type === 'white',
         },
-        this.buttonFullWidth ? 'w-full justify-between' : 'justify-center'
+        this.fullWidth ? 'w-full justify-between' : 'justify-center'
       ]
     },
     isDisabled() {
