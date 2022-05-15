055e491e2e50292728eca7b4e4ae08fbe1374826
Faris Ansari <netchamp.faris@gmail.com>
2022-05-16 03:39:34 +0530

    feat(Button): success and warning appearance types
    - show focus ring only when using keyboard

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,6 +11,8 @@
         'text-white': appearance == 'primary',
         'text-gray-600': appearance == 'secondary',
         'text-red-200': appearance == 'danger',
+        'text-green-200': appearance == 'success',
+        'text-yellow-200': appearance == 'warning',
       }"
     />
     <FeatherIcon
@@ -40,7 +42,15 @@
 import FeatherIcon from './FeatherIcon.vue'
 import LoadingIndicator from './LoadingIndicator.vue'
 
-const ValidAppearances = ['primary', 'secondary', 'danger', 'white', 'minimal']
+const ValidAppearances = [
+  'primary',
+  'secondary',
+  'danger',
+  'success',
+  'warning',
+  'white',
+  'minimal',
+]
 
 export default {
   name: 'Button',
@@ -98,19 +108,23 @@ export default {
     buttonClasses() {
       let appearanceClasses = {
         primary:
-          'bg-blue-500 hover:bg-blue-600 text-white focus:ring-2 focus:ring-offset-2 focus:ring-blue-500',
+          'bg-blue-500 hover:bg-blue-600 border-transparent text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500',
         secondary:
-          'bg-gray-100 hover:bg-gray-200 text-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500',
+          'bg-gray-100 hover:bg-gray-200 border-transparent text-gray-900 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-500',
         danger:
-          'bg-red-500 hover:bg-red-400 text-white focus:ring-2 focus:ring-offset-2 focus:ring-red-500',
+          'bg-red-500 hover:bg-red-400 border-transparent text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500',
+        success:
+          'bg-green-500 hover:bg-green-400 border-transparent text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500',
+        warning:
+          'bg-yellow-500 hover:bg-yellow-400 border-transparent text-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-yellow-500',
         white:
-          'bg-white text-gray-900 border hover:bg-gray-50 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400',
+          'bg-white text-gray-900 border-gray-200 hover:bg-gray-50 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-gray-400',
         minimal: `active:bg-gray-200 focus:bg-gray-200 text-gray-900 ${
           this.active ? 'bg-gray-200' : 'bg-transparent hover:bg-gray-200'
         }`,
       }
       return [
-        'inline-flex items-center justify-center text-base leading-5 rounded-md transition-colors focus:outline-none',
+        'inline-flex items-center justify-center text-base leading-5 rounded-md border transition-colors focus:outline-none',
         this.icon ? 'p-1.5' : 'px-3 py-1',
         this.isDisabled
           ? 'opacity-50 cursor-not-allowed pointer-events-none'
