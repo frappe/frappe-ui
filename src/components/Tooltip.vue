b5692cf7427b2fe21874905cc1a082c6d6492bd9
Faris Ansari <netchamp.faris@gmail.com>
2022-08-07 22:35:22 +0530

    feat: Tooltip

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,35 @@
+<template>
+  <Popover trigger="hover" :hoverDelay="hoverDelay" :placement="placement">
+    <template #target>
+      <slot />
+    </template>
+    <template #body>
+      <slot name="body">
+        <div
+          class="px-2 py-1 text-xs text-white bg-gray-800 border border-gray-100 rounded-lg shadow-xl"
+        >
+          {{ text }}
+        </div>
+      </slot>
+    </template>
+  </Popover>
+</template>
+<script>
+import Popover from './Popover.vue'
+export default {
+  name: 'Tooltip',
+  components: { Popover },
+  props: {
+    hoverDelay: {
+      default: 0.5,
+    },
+    placement: {
+      default: 'bottom-start',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
