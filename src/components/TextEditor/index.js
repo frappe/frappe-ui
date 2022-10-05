a12f6fdfae29e3acb751fc76a7df0469c61711c3
Faris Ansari <netchamp.faris@gmail.com>
2022-10-05 20:19:38 +0530

    fix: export texteditor supporting components

--- src/components/TextEditor/index.js
+++ src/components/TextEditor/index.js
@@ -1,1 +1,5 @@
 export { default } from './TextEditor.vue'
+export { default as TextEditor } from './TextEditor.vue'
+export { default as TextEditorBubbleMenu } from './TextEditorBubbleMenu.vue'
+export { default as TextEditorFixedMenu } from './TextEditorFixedMenu.vue'
+export { default as TextEditorFloatingMenu } from './TextEditorFloatingMenu.vue'
