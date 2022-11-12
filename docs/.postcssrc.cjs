6146f5e087dc2604ac5ae3acac7c6d4c3173e1ff
Faris Ansari <netchamp.faris@gmail.com>
2022-11-12 21:04:14 +0530

    chore: tailwindcss setup

---
+++ docs/.postcssrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './docs/tailwind.config.docs.js' },
+    autoprefixer: {},
+  },
+}
---
+++ docs/tailwind.config.docs.js
@@ -0,0 +1,7 @@
+// const
+module.exports = {
+  presets: [require('../src/utils/tailwind.config')],
+  content: ['./docs/**/*.md', './src/components/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {},
+  plugins: [],
+}
