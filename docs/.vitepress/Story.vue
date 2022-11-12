e4143cf5c9311a5739053eb0e8e5505bf20b387a
Faris Ansari <netchamp.faris@gmail.com>
2022-11-12 21:04:48 +0530

    fix: Story component for demos

---
+++ docs/.vitepress/Story.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div class="flex w-full items-center justify-center rounded-lg border p-4">
+    <div>
+      <slot name="code" />
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,12 @@
+import DefaultTheme from 'vitepress/theme'
+import Story from '../Story.vue'
+import '../../../src/style.css'
+import './style.css'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx)
+    ctx.app.component('Story', Story)
+  },
+}
