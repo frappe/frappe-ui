073a88620122ddedc822bd8ccf935dd65b2d1116
Faris Ansari <netchamp.faris@gmail.com>
2023-07-19 19:47:29 +0530

    chore: button story

---
+++ src/components/Button.story.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import Button from './Button.vue'
+const state = reactive({
+  theme: '',
+  size: '',
+  label: 'Button',
+  loading: false,
+  loadingText: null,
+  disabled: false,
+  link: null,
+})
+const variants = ['solid', 'subtle', 'outline', 'ghost']
+const themes = ['gray', 'blue', 'green', 'red']
+const sizes = ['sm', 'md', 'lg', 'xl', '2xl']
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: 300 }">
+    <Variant v-for="variant in variants" :key="variant" :title="variant">
+      <div class="p-4">
+        <Button :variant="variant">{{ state.label }}</Button>
+      </div>
+    </Variant>
+
+    <template #controls>
+      <!-- <HstText v-model="state.label" title="Content" />
+      <HstCheckbox v-model="state.disabled" title="Disabled" /> -->
+    </template>
+  </Story>
+</template>
+<docs lang="md">
+# Button
+
+The Button component is used to trigger an action such as submitting a form,
+opening a Dialog, or canceling an action.
+</docs>
---
+++ src/components/Introduction.story.md
@@ -0,0 +1,12 @@
+---
+group: 'top'
+icon: 'carbon:bookmark'
+---
+
+# Welcome
+
+This is a demo book using Vue 3.
+
+---
+
+Learn more about Histoire [here](https://histoire.dev/).
