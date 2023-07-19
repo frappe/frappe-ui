a8275e76161b8d9d5f880c7d5212b3070971a826
Arjun <contact@arjunchoudhary.com>
2023-07-19 18:17:01 +0530

    fix: Update `vite-dev-server.js` Node IP resolution fix
    Node more correctly follows OS native [`dns.lookup()`](https://github.com/nodejs/node/pull/39987) resulting in this breakage

--- src/utils/vite-dev-server.js
+++ src/utils/vite-dev-server.js
@@ -2,7 +2,7 @@ module.exports = {
   getProxyOptions({ port }) {
     return {
       '^/(app|login|api|assets|files)': {
-        target: `http://localhost:${port}`,
+        target: `http://127.0.0.1:${port}`,
         ws: true,
         router: function (req) {
           const site_name = req.headers.host.split(':')[0]
