19db8bc3a7463c11e4ef3b11ff7641371701bb47
Shariq Ansari <sharique.rik@gmail.com>
2024-01-07 16:53:00 +0530

    fix: added load more button

--- src/components/ListView/ListFooter.vue
+++ src/components/ListView/ListFooter.vue
@@ -3,15 +3,23 @@
     <slot>
       <slot name="left">
         <TabButtons
-          v-model="pageLength"
+          v-model="pageLengthCount"
           :buttons="pageLengthOptions.map((o) => ({ label: o, value: o }))"
         />
       </slot>
       <slot name="right">
-        <div class="flex items-center gap-1 text-base text-gray-600">
-          <div>{{ options.rowCount || '0' }}</div>
-          <div>of</div>
-          <div>{{ options.totalCount || '0' }}</div>
+        <div class="flex items-center">
+          <Button
+            v-if="showLoadMore"
+            label="Load More"
+            @click="emit('loadMore')"
+          />
+          <div v-if="showLoadMore" class="mx-3 h-[80%] border-l" />
+          <div class="flex items-center gap-1 text-base text-gray-600">
+            <div>{{ options.rowCount || '0' }}</div>
+            <div>of</div>
+            <div>{{ options.totalCount || '0' }}</div>
+          </div>
         </div>
       </slot>
     </slot>
@@ -36,12 +44,20 @@ const props = defineProps({
   },
 })
 
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue', 'loadMore'])
 
-const pageLength = computed({
+const pageLengthCount = computed({
   get: () => props.modelValue,
   set: (value) => emit('update:modelValue', value),
 })
 
 const pageLengthOptions = ref(props.options.pageLengthOptions || [20, 50, 100])
+
+const showLoadMore = computed(() => {
+  return (
+    props.options.rowCount &&
+    props.options.totalCount &&
+    props.options.rowCount < props.options.totalCount
+  )
+})
 </script>
