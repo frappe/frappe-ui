2fadec96a01f0dbd3f4e1de9faae8a4f912a6bc9
David <dgx.arnold@gmail.com>
2024-03-24 15:38:12 +0100

    feat: make url path regex configurable

--- vite.js
+++ vite.js
@@ -1,27 +1,30 @@
 const path = require('path')
 const fs = require('fs')
 
-module.exports = function proxyOptions({ port = 8080 } = {}) {
+module.exports = function proxyOptions({
+  port = 8080,
+  regex = '^/(app|login|api|assets|files)',
+ } = {}) {
   const config = getCommonSiteConfig()
   const webserver_port = config ? config.webserver_port : 8000
   if (!config) {
     console.log('No common_site_config.json found, using default port 8000')
+  }
+  let proxy = {}
+  proxy[regex] = {
+    target: `http://127.0.0.1:${webserver_port}`,
+    ws: true,
+    router: function (req) {
+      const site_name = req.headers.host.split(':')[0]
+      return `http://${site_name}:${webserver_port}`
+    }
   }
   return {
     name: 'frappeui-vite-plugin',
     config: () => ({
       server: {
         port: port,
-        proxy: {
-          '^/(app|login|api|assets|files|pages)': {
-            target: `http://127.0.0.1:${webserver_port}`,
-            ws: true,
-            router: function (req) {
-              const site_name = req.headers.host.split(':')[0]
-              return `http://${site_name}:${webserver_port}`
-            },
-          },
-        },
+        proxy: proxy,
       },
     }),
   }
