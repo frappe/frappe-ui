00427430f49d47007dfb1ec6b7a32f5c98d1b997
Gursheen Anand <gursheen@frappe.io>
2024-10-16 18:54:04 +0530

    refactor: add types to Autocomplete

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -63,7 +63,7 @@
                   />
                   <button
                     class="absolute right-0 inline-flex h-7 w-7 items-center justify-center"
-                    @click="selectedValue = null"
+                    @click="selectedValue = []"
                   >
                     <FeatherIcon name="x" class="w-4" />
                   </button>
@@ -164,185 +164,201 @@
   </Combobox>
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed, ref, nextTick, watch } from 'vue'
 import {
   Combobox,
-  ComboboxButton,
   ComboboxInput,
   ComboboxOption,
   ComboboxOptions,
 } from '@headlessui/vue'
-import { nextTick } from 'vue'
 import Popover from './Popover.vue'
 import Button from './Button.vue'
 import FeatherIcon from './FeatherIcon.vue'
 
-export default {
-  name: 'Autocomplete',
-  props: {
-    options: {
-      type: Array,
-      default: () => [],
-    },
-    modelValue: {
-      type: [String, Object, Array],
-    },
-    placeholder: {
-      type: String,
-    },
-    bodyClasses: {
-      type: [String, Array, Object],
-    },
-    multiple: {
-      type: Boolean,
-      default: false,
-    },
-    hideSearch: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  emits: ['update:modelValue', 'update:query', 'change'],
-  components: {
-    Popover,
-    Button,
-    FeatherIcon,
-    Combobox,
-    ComboboxInput,
-    ComboboxOptions,
-    ComboboxOption,
-    ComboboxButton,
-  },
-  expose: ['togglePopover', 'rootRef'],
-  data() {
-    return {
-      query: '',
-      showOptions: false,
+type AutocompleteOption = {
+  label: string
+  value: string
+  group?: string
+  hideLabel?: boolean
+  items?: AutocompleteOption[]
+  description?: string
+}
+
+interface AutocompleteProps {
+  modelValue: string | string[] | AutocompleteOption | AutocompleteOption[]
+  options?: string[] | AutocompleteOption[]
+  placeholder?: string
+  bodyClasses?: string | string[]
+  multiple?: boolean
+  hideSearch?: boolean
+}
+
+const props = withDefaults(defineProps<AutocompleteProps>(), {
+  multiple: false,
+  hideSearch: false,
+})
+
+const emit = defineEmits(['update:modelValue', 'update:query', 'change'])
+
+const searchInput = ref()
+
+const query = ref('')
+
+const showOptions = ref(false)
+
+const groups = computed(() => {
+  if (!props.options || props.options.length == 0) return []
+
+  return props.options.map((option, i) => {
+    if (typeof option === 'string') {
+      return {
+        key: i,
+        group: '',
+        hideLabel: false,
+        items: [{ label: option, value: option }],
+      }
     }
-  },
-  computed: {
-    selectedValue: {
-      get() {
-        if (!this.multiple) {
-          return this.findOption(this.modelValue)
-        }
-        // in case of `multiple`, modelValue is an array of values
-        // if the modelValue is a list of values, convert them to options
-        return isOptionOrValue(this.modelValue?.[0]) === 'value'
-          ? this.modelValue?.map((v) => this.findOption(v))
-          : this.modelValue
-      },
-      set(val) {
-        this.query = ''
-        if (val && !this.multiple) this.showOptions = false
-        if (!this.multiple) {
-          this.$emit('update:modelValue', val)
-          return
-        }
-        this.$emit('update:modelValue', val)
-      },
-    },
-    groups() {
-      if (!this.options || this.options.length == 0) return []
+    return {
+      key: i,
+      group: option.group || '',
+      hideLabel: option.hideLabel || false,
+      items: filterOptions(sanitizeOptions(option.items || [option])),
+    }
+  })
+})
 
-      let groups = this.options[0]?.group
-        ? this.options
-        : [{ group: '', items: this.sanitizeOptions(this.options) }]
-
-      return groups
-        .map((group, i) => {
-          return {
-            key: i,
-            group: group.group,
-            hideLabel: group.hideLabel || false,
-            items: this.filterOptions(this.sanitizeOptions(group.items)),
-          }
-        })
-        .filter((group) => group.items.length > 0)
-    },
-    allOptions() {
-      return this.groups.flatMap((group) => group.items)
-    },
-    areAllOptionsSelected() {
-      if (!this.multiple) return false
-      return this.allOptions.length === this.selectedValue?.length
-    },
-  },
-  watch: {
-    query(q) {
-      this.$emit('update:query', q)
-    },
-    showOptions(val) {
-      if (val) nextTick(() => this.$refs.searchInput?.$el?.focus())
-    },
-  },
-  methods: {
-    rootRef() {
-      return this.$refs['rootRef']
-    },
-    togglePopover(val) {
-      this.showOptions = val ?? !this.showOptions
-    },
-    findOption(option) {
-      if (!option) return option
-      const value = isOptionOrValue(option) === 'value' ? option : option.value
-      return this.allOptions.find((o) => o.value === value)
-    },
-    filterOptions(options) {
-      if (!this.query) return options
-      return options.filter((option) => {
-        return (
-          option.label
-            .toLowerCase()
-            .includes(this.query.trim().toLowerCase()) ||
-          option.value.toLowerCase().includes(this.query.trim().toLowerCase())
-        )
-      })
-    },
-    displayValue(option) {
-      if (!option) return ''
-
-      if (!this.multiple) {
-        return this.getLabel(this.findOption(option))
-      }
-
-      if (!Array.isArray(option)) return ''
-
-      // in case of `multiple`, option is an array of values
-      // so the display value should be comma separated labels
-      return option.map((v) => this.getLabel(this.findOption(v))).join(', ')
-    },
-    getLabel(option) {
-      if (isOptionOrValue(option) === 'value') return option
-      return option?.label || option?.value || 'No label'
-    },
-    sanitizeOptions(options) {
-      if (!options) return []
-      // in case the options are just values, convert them to objects
-      return options.map((option) => {
-        return isOptionOrValue(option) === 'option'
-          ? option
-          : { label: option, value: option }
-      })
-    },
-    isOptionSelected(option) {
-      if (!this.selectedValue) return false
-      const value = isOptionOrValue(option) === 'value' ? option : option.value
-      if (!this.multiple) {
-        return this.selectedValue?.value === value
-      }
-      return this.selectedValue?.find((v) => v && v.value === value)
-    },
-    selectAll() {
-      this.selectedValue = this.allOptions
-    },
-    clearAll() {
-      this.selectedValue = []
-    },
-  },
+const sanitizeOptions = (
+  options: string[] | AutocompleteOption[],
+): AutocompleteOption[] => {
+  if (!options) return []
+  // in case the options are just values, convert them to objects
+  return options.map((option) => {
+    if (isAutocompleteOption(option)) {
+      return option as AutocompleteOption
+    }
+    return { label: option.toString(), value: option.toString() }
+  })
 }
 
-function isOptionOrValue(optionOrValue) {
-  return typeof optionOrValue === 'object' ? 'option' : 'value'
+const filterOptions = (options: AutocompleteOption[]) => {
+  if (!query.value) return options
+  return options.filter((option) => {
+    return (
+      option.label.toLowerCase().includes(query.value.trim().toLowerCase()) ||
+      option.value.toLowerCase().includes(query.value.trim().toLowerCase())
+    )
+  })
 }
+
+const allOptions = computed(() => {
+  return groups.value.flatMap((group) => group.items)
+})
+
+const selectedValue = computed({
+  get() {
+    if (!props.multiple) {
+      return findOption(props.modelValue as string | AutocompleteOption)
+    }
+    // in case of `multiple`, modelValue is an array of values
+    // if the modelValue is a list of values, convert them to options
+    let values = props.modelValue as string[] | AutocompleteOption[]
+    if (!values) return []
+    return isAutocompleteOption(values[0])
+      ? values
+      : values?.map((v) => findOption(v))
+  },
+  set(val) {
+    query.value = ''
+    if (val && !props.multiple) showOptions.value = false
+    if (!props.multiple) {
+      emit('update:modelValue', val)
+      return
+    }
+    emit('update:modelValue', val)
+  },
+})
+
+const findOption = (option: AutocompleteOption | string) => {
+  if (!option) return option
+  const value = isAutocompleteOption(option) ? option.value : option
+  return allOptions.value.find((o) => o.value === value)
+}
+
+const isAutocompleteOption = (optionOrValue: string | AutocompleteOption) => {
+  return typeof optionOrValue === 'object'
+}
+
+const getLabel = (option: string | AutocompleteOption) => {
+  if (isAutocompleteOption(option)) {
+    return option?.label || option?.value || 'No label'
+  }
+  return option
+}
+
+const displayValue = (option: any) => {
+  if (!option) return ''
+
+  if (!props.multiple) {
+    return getLabel(findOption(option) || '')
+  }
+
+  if (!Array.isArray(option)) return ''
+
+  // in case of `multiple`, option is an array of values
+  // so the display value should be comma separated labels
+  return option.map((v) => getLabel(findOption(v) || '')).join(', ')
+}
+
+const isOptionSelected = (option: AutocompleteOption) => {
+  if (!selectedValue.value) return false
+  const value = isAutocompleteOption(option) ? option.value : option
+  if (!props.multiple) {
+    return selectedValue.value === value
+  }
+  return (selectedValue.value as AutocompleteOption[]).find(
+    (v) => v && v.value === value,
+  )
+}
+
+const areAllOptionsSelected = computed(() => {
+  if (!props.multiple) return false
+  return (
+    allOptions.value.length ===
+    (selectedValue.value as string[] | AutocompleteOption[])?.length
+  )
+})
+
+const selectAll = () => {
+  selectedValue.value = allOptions.value
+}
+
+const clearAll = () => {
+  selectedValue.value = []
+}
+
+watch(
+  () => query.value,
+  () => {
+    emit('update:query', query.value)
+  },
+)
+
+watch(
+  () => showOptions.value,
+  (val) => {
+    if (val) nextTick(() => searchInput.value?.$el.focus())
+  },
+)
+
+const rootRef = ref()
+
+const togglePopover = () => {
+  showOptions.value = !showOptions.value
+}
+
+defineExpose({
+  rootRef,
+  togglePopover,
+})
 </script>
