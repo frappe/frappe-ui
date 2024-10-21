c78da5f86a25014159759d19c0f459908a36e022
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-10-21 20:24:06 +0530

    refactor: rewrite DateRangePicker, move common date functions to utils

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -76,14 +76,14 @@
           >
             <div
               v-for="date in week"
-              :key="toValue(date)"
+              :key="getDateValue(date)"
               class="flex h-8 w-8 cursor-pointer items-center justify-center rounded hover:bg-gray-50"
               :class="{
                 'text-gray-400': date.getMonth() !== currentMonth - 1,
                 'font-extrabold text-gray-900':
-                  toValue(date) === toValue(today),
+                  getDateValue(date) === getDateValue(today),
                 'bg-gray-800 text-white hover:bg-gray-800':
-                  toValue(date) === dateValue,
+                  getDateValue(date) === dateValue,
               }"
               @click="
                 () => {
@@ -121,6 +121,13 @@ import Button from './Button.vue'
 import Popover from './Popover.vue'
 import FeatherIcon from './FeatherIcon.vue'
 import TextInput from './TextInput.vue'
+
+import {
+  getDate,
+  getDateValue,
+  getDatesAfter,
+  getDaysInMonth,
+} from '../utils/dates'
 
 interface DatePickerProps {
   value?: string
@@ -203,29 +210,8 @@ const dateValue = computed(() => {
 })
 
 function selectDate(date: Date) {
-  emit('change', toValue(date))
-  emit('update:modelValue', toValue(date))
-}
-
-type DateConstructorParam = string | number | Date
-
-function getDate(...args: DateConstructorParam[]): Date {
-  return new Date(...(args as [DateConstructorParam]))
-}
-
-function toValue(date: Date) {
-  if (!date) {
-    return ''
-  }
-
-  // toISOString is buggy and reduces the day by one
-  // this is because it considers the UTC timestamp
-  // in order to circumvent that we need to use luxon/moment
-  // but that refactor could take some time, so fixing the time difference
-  // as suggested in this answer.
-  // https://stackoverflow.com/a/16084846/3541205
-  date.setHours(0, -date.getTimezoneOffset(), 0, 0)
-  return date.toISOString().slice(0, 10)
+  emit('change', getDateValue(date))
+  emit('update:modelValue', getDateValue(date))
 }
 
 function prevMonth() {
@@ -246,46 +232,6 @@ function changeMonth(adder: number) {
     currentMonth.value = 1
     currentYear.value = currentYear.value + 1
   }
-}
-
-function getDatesAfter(date: Date, count: number) {
-  let incrementer = 1
-  if (count < 0) {
-    incrementer = -1
-    count = Math.abs(count)
-  }
-  const dates: Date[] = []
-
-  while (count) {
-    date = getDate(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate() + incrementer,
-    )
-    dates.push(date)
-    count--
-  }
-
-  if (incrementer === -1) {
-    return dates.reverse()
-  }
-  return dates
-}
-
-function getDaysInMonth(monthIndex: number, year: number) {
-  const daysInMonthMap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  const daysInMonth = daysInMonthMap[monthIndex]
-  if (monthIndex === 1 && isLeapYear(year)) {
-    return 29
-  }
-  return daysInMonth
-}
-
-function isLeapYear(year: number) {
-  if (year % 400 === 0) return true
-  if (year % 100 === 0) return false
-  if (year % 4 === 0) return true
-  return false
 }
 
 function selectCurrentMonthYear() {
--- src/components/DateRangePicker.vue
+++ src/components/DateRangePicker.vue
@@ -29,7 +29,7 @@
             />
           </Button>
           <div class="flex-1 text-center text-base font-medium text-gray-700">
-            {{ formatMonth }}
+            {{ formattedMonth }}
           </div>
           <Button variant="ghost" class="h-7 w-7" @click="nextMonth">
             <FeatherIcon
@@ -62,18 +62,18 @@
           >
             <div
               v-for="date in week"
-              :key="toValue(date)"
+              :key="getDateValue(date)"
               class="flex h-8 w-8 cursor-pointer items-center justify-center rounded hover:bg-gray-50"
               :class="{
                 'text-gray-400': date.getMonth() !== currentMonth - 1,
                 'text-gray-900': date.getMonth() === currentMonth - 1,
                 'font-extrabold text-gray-900':
-                  toValue(date) === toValue(today),
+                  getDateValue(date) === getDateValue(today),
                 'rounded-none bg-gray-100': isInRange(date),
                 'rounded-l-md rounded-r-none bg-gray-800 text-white hover:bg-gray-800':
-                  fromDate && toValue(date) === toValue(fromDate),
+                  fromDate && getDateValue(date) === getDateValue(fromDate),
                 'rounded-r-md bg-gray-800 text-white hover:bg-gray-800':
-                  toDate && toValue(date) === toValue(toDate),
+                  toDate && getDateValue(date) === getDateValue(toDate),
               }"
               @click="() => handleDateClick(date)"
             >
@@ -84,14 +84,24 @@
         <div class="flex justify-end space-x-1 p-1">
           <Button
             :label="'Clear'"
-            @click="() => clearDates() | togglePopover()"
+            @click="
+              () => {
+                clearDates()
+                togglePopover()
+              }
+            "
             :disabled="!fromDate || !toDate"
           />
           <Button
             variant="solid"
             :label="'Apply'"
             :disabled="!fromDate || !toDate"
-            @click="() => selectDates() | togglePopover()"
+            @click="
+              () => {
+                selectDates()
+                togglePopover()
+              }
+            "
           />
         </div>
       </div>
@@ -99,248 +109,190 @@
   </Popover>
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed, ref, onMounted } from 'vue'
+
 import Input from './Input.vue'
 import Button from './Button.vue'
 import Popover from './Popover.vue'
 import FeatherIcon from './FeatherIcon.vue'
 import TextInput from './TextInput.vue'
-export default {
-  name: 'DateRangePicker',
-  props: {
-    value: {
-      type: String,
-    },
-    modelValue: {
-      type: String,
-    },
-    placeholder: {
-      type: String,
-    },
-    formatter: {
-      type: Function,
-      default: null,
-    },
-    readonly: {
-      type: Boolean,
-    },
-    inputClass: {
-      type: [String, Array, Object],
-    },
-  },
-  emits: ['update:modelValue', 'change'],
-  components: {
-    Popover,
-    Input,
-    Button,
-    FeatherIcon,
-    TextInput,
-  },
-  data() {
-    const fromDate = this.dateValue ? this.dateValue[0] : ''
-    const toDate = this.dateValue ? this.dateValue[1] : ''
-    return {
-      currentYear: null,
-      currentMonth: null,
-      fromDate,
-      toDate,
-    }
-  },
-  created() {
-    this.selectCurrentMonthYear()
-  },
-  computed: {
-    today() {
-      return this.getDate()
-    },
-    datesAsWeeks() {
-      let datesAsWeeks = []
-      let dates = this.dates.slice()
-      while (dates.length) {
-        let week = dates.splice(0, 7)
-        datesAsWeeks.push(week)
-      }
-      return datesAsWeeks
-    },
-    dates() {
-      if (!(this.currentYear && this.currentMonth)) {
-        return []
-      }
-      let monthIndex = this.currentMonth - 1
-      let year = this.currentYear
 
-      let firstDayOfMonth = this.getDate(year, monthIndex, 1)
-      let lastDayOfMonth = this.getDate(year, monthIndex + 1, 0)
-      let leftPaddingCount = firstDayOfMonth.getDay()
-      let rightPaddingCount = 6 - lastDayOfMonth.getDay()
+import {
+  getDate,
+  getDateValue,
+  getDatesAfter,
+  getDaysInMonth,
+} from '../utils/dates'
 
-      let leftPadding = this.getDatesAfter(firstDayOfMonth, -leftPaddingCount)
-      let rightPadding = this.getDatesAfter(lastDayOfMonth, rightPaddingCount)
-      let daysInMonth = this.getDaysInMonth(monthIndex, year)
-      let datesInMonth = this.getDatesAfter(firstDayOfMonth, daysInMonth - 1)
-
-      let dates = [
-        ...leftPadding,
-        firstDayOfMonth,
-        ...datesInMonth,
-        ...rightPadding,
-      ]
-      if (dates.length < 42) {
-        const finalPadding = this.getDatesAfter(dates.at(-1), 42 - dates.length)
-        dates = dates.concat(...finalPadding)
-      }
-      return dates
-    },
-    formatMonth() {
-      let date = this.getDate(this.currentYear, this.currentMonth - 1, 1)
-      let month = date.toLocaleString('en-US', {
-        month: 'long',
-      })
-      return `${month}, ${date.getFullYear()}`
-    },
-    dateValue() {
-      return this.value ? this.value : this.modelValue
-    },
-  },
-  methods: {
-    handleDateClick(date) {
-      if (this.fromDate && this.toDate) {
-        this.fromDate = this.toValue(date)
-        this.toDate = ''
-      } else if (this.fromDate && !this.toDate) {
-        this.toDate = this.toValue(date)
-      } else {
-        this.fromDate = this.toValue(date)
-      }
-      this.swapDatesIfNecessary()
-    },
-    selectDates() {
-      let val = `${this.fromDate},${this.toDate}`
-      if (!this.fromDate && !this.toDate) {
-        val = ''
-      }
-      this.$emit('change', val)
-      this.$emit('update:modelValue', val)
-    },
-    swapDatesIfNecessary() {
-      if (!this.fromDate || !this.toDate) {
-        return
-      }
-      // if fromDate is greater than toDate, swap them
-      let fromDate = this.getDate(this.fromDate)
-      let toDate = this.getDate(this.toDate)
-      if (fromDate > toDate) {
-        let temp = fromDate
-        fromDate = toDate
-        toDate = temp
-      }
-      this.fromDate = this.toValue(fromDate)
-      this.toDate = this.toValue(toDate)
-    },
-    selectCurrentMonthYear() {
-      let date = this.toDate ? this.getDate(this.toDate) : this.today
-      this.currentYear = date.getFullYear()
-      this.currentMonth = date.getMonth() + 1
-    },
-    prevMonth() {
-      this.changeMonth(-1)
-    },
-    nextMonth() {
-      this.changeMonth(1)
-    },
-    changeMonth(adder) {
-      this.currentMonth = this.currentMonth + adder
-      if (this.currentMonth < 1) {
-        this.currentMonth = 12
-        this.currentYear = this.currentYear - 1
-      }
-      if (this.currentMonth > 12) {
-        this.currentMonth = 1
-        this.currentYear = this.currentYear + 1
-      }
-    },
-    getDatesAfter(date, count) {
-      let incrementer = 1
-      if (count < 0) {
-        incrementer = -1
-        count = Math.abs(count)
-      }
-      let dates = []
-      while (count) {
-        date = this.getDate(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + incrementer,
-        )
-        dates.push(date)
-        count--
-      }
-      if (incrementer === -1) {
-        return dates.reverse()
-      }
-      return dates
-    },
-
-    getDaysInMonth(monthIndex, year) {
-      let daysInMonthMap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-      let daysInMonth = daysInMonthMap[monthIndex]
-      if (monthIndex === 1 && this.isLeapYear(year)) {
-        return 29
-      }
-      return daysInMonth
-    },
-
-    isLeapYear(year) {
-      if (year % 400 === 0) return true
-      if (year % 100 === 0) return false
-      if (year % 4 === 0) return true
-      return false
-    },
-
-    toValue(date) {
-      if (!date) {
-        return ''
-      }
-      if (typeof date === 'string') {
-        return date
-      }
-
-      // toISOString is buggy and reduces the day by one
-      // this is because it considers the UTC timestamp
-      // in order to circumvent that we need to use luxon/moment
-      // but that refactor could take some time, so fixing the time difference
-      // as suggested in this answer.
-      // https://stackoverflow.com/a/16084846/3541205
-      date.setHours(0, -date.getTimezoneOffset(), 0, 0)
-      return date.toISOString().slice(0, 10)
-    },
-
-    getDate(...args) {
-      let d = new Date(...args)
-      return d
-    },
-
-    isInRange(date) {
-      if (!this.fromDate || !this.toDate) {
-        return false
-      }
-      return (
-        date >= this.getDate(this.fromDate) && date <= this.getDate(this.toDate)
-      )
-    },
-
-    formatDates(value) {
-      if (!value) {
-        return ''
-      }
-      const values = value.split(',')
-      return this.formatter(values[0]) + ' to ' + this.formatter(values[1])
-    },
-    clearDates() {
-      this.fromDate = ''
-      this.toDate = ''
-      this.selectDates()
-    },
-  },
+interface DateRangePickerProps {
+  value?: string
+  modelValue?: string
+  placeholder?: string
+  formatter?: (date: string) => string
+  readonly?: boolean
+  inputClass?: string | Array<string> | Record<string, boolean>
 }
+
+const props = withDefaults(defineProps<DateRangePickerProps>(), {
+  formatter: undefined,
+})
+
+const emit = defineEmits(['update:modelValue', 'change'])
+
+const currentYear = ref<number>(0)
+const currentMonth = ref<number>(0)
+
+const dateValue = computed(() => {
+  return props.value ? props.value : props.modelValue
+})
+
+const fromDate = ref<string>(dateValue.value ? dateValue.value[0] : '')
+const toDate = ref<string>(dateValue.value ? dateValue.value[1] : '')
+
+const today = computed(() => getDate())
+
+const dates = computed(() => {
+  if (!(currentYear.value && currentMonth.value)) {
+    return []
+  }
+  const monthIndex = currentMonth.value - 1
+  const year = currentYear.value
+
+  const firstDayOfMonth = getDate(year, monthIndex, 1)
+  const lastDayOfMonth = getDate(year, monthIndex + 1, 0)
+  const leftPaddingCount = firstDayOfMonth.getDay()
+  const rightPaddingCount = 6 - lastDayOfMonth.getDay()
+
+  const leftPadding = getDatesAfter(firstDayOfMonth, -leftPaddingCount)
+  const rightPadding = getDatesAfter(lastDayOfMonth, rightPaddingCount)
+  const daysInMonth = getDaysInMonth(monthIndex, year)
+  const datesInMonth = getDatesAfter(firstDayOfMonth, daysInMonth - 1)
+
+  let dates = [
+    ...leftPadding,
+    firstDayOfMonth,
+    ...datesInMonth,
+    ...rightPadding,
+  ]
+
+  if (dates.length < 42) {
+    const lastDate = dates.at(-1)
+    if (lastDate) {
+      const finalPadding = getDatesAfter(lastDate, 42 - dates.length)
+      dates = dates.concat(...finalPadding)
+    }
+  }
+  return dates
+})
+
+const datesAsWeeks = computed(() => {
+  const datesAsWeeks: Date[][] = []
+  const computedDates = dates.value.slice()
+  while (computedDates.length) {
+    const week = computedDates.splice(0, 7)
+    datesAsWeeks.push(week)
+  }
+  return datesAsWeeks
+})
+
+const formattedMonth = computed(() => {
+  if (!(currentYear.value && currentMonth.value)) {
+    return ''
+  }
+  const date = getDate(currentYear.value, currentMonth.value - 1, 1)
+  const month = date.toLocaleString('en-US', {
+    month: 'long',
+  })
+  return `${month}, ${date.getFullYear()}`
+})
+
+function handleDateClick(date: Date) {
+  if (fromDate.value && toDate.value) {
+    fromDate.value = getDateValue(date)
+    toDate.value = ''
+  } else if (fromDate.value && !toDate.value) {
+    toDate.value = getDateValue(date)
+  } else {
+    fromDate.value = getDateValue(date)
+  }
+  swapDatesIfNecessary()
+}
+
+function swapDatesIfNecessary() {
+  if (!fromDate.value || !toDate.value) {
+    return
+  }
+
+  // if fromDate is greater than toDate, swap them
+  let from = getDate(fromDate.value)
+  let to = getDate(toDate.value)
+  if (from > to) {
+    let temp = from
+    from = to
+    to = temp
+  }
+  fromDate.value = getDateValue(from)
+  toDate.value = getDateValue(to)
+}
+
+function selectDates() {
+  let val = `${fromDate.value},${toDate.value}`
+  if (!fromDate.value && !toDate.value) {
+    val = ''
+  }
+  emit('change', val)
+  emit('update:modelValue', val)
+}
+
+function prevMonth() {
+  changeMonth(-1)
+}
+
+function nextMonth() {
+  changeMonth(1)
+}
+
+function changeMonth(adder: number) {
+  currentMonth.value = currentMonth.value + adder
+  if (currentMonth.value < 1) {
+    currentMonth.value = 12
+    currentYear.value = currentYear.value - 1
+  }
+  if (currentMonth.value > 12) {
+    currentMonth.value = 1
+    currentYear.value = currentYear.value + 1
+  }
+}
+
+function selectCurrentMonthYear() {
+  let date = toDate.value ? getDate(toDate.value) : today.value
+  currentYear.value = date.getFullYear()
+  currentMonth.value = date.getMonth() + 1
+}
+
+function isInRange(date: Date) {
+  if (!fromDate.value || !toDate.value) {
+    return false
+  }
+  return date >= getDate(fromDate.value) && date <= getDate(toDate.value)
+}
+
+function formatDates(value: string) {
+  if (!value) {
+    return ''
+  }
+  const values = value.split(',')
+  return props.formatter
+    ? props.formatter(values[0]) + ' to ' + props.formatter(values[1])
+    : value
+}
+
+function clearDates() {
+  fromDate.value = ''
+  toDate.value = ''
+  selectDates()
+}
+
+onMounted(() => selectCurrentMonthYear())
 </script>
