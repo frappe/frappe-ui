c16b27e00b5f13e73732ab0c2cfda3f88ed252f0
ruchamahabal <ruchamahabal@users.noreply.github.com>
2024-10-28 05:18:10 +0000

    deploy: 2d4dde894103128888958632a75f9a24ff771ab6

---
+++ assets/InsertImage-a29d4bb9.js
@@ -0,0 +1,140 @@
+import { f as fileToBase64 } from "./ListView.story-026df16a.js";
+import { D as Dialog } from "./Dialog-4bafa913.js";
+import { B as Button } from "./Button-1d9c794b.js";
+import { aA as resolveComponent, aB as openBlock, aG as createElementBlock, aT as renderSlot, aU as normalizeProps, aV as guardReactiveProps, aE as createVNode, aD as withCtx, aF as createBaseVNode, aN as toDisplayString, aH as createCommentVNode, aM as createTextVNode, aK as Fragment } from "./vendor-ff760363.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-cc2b3d55.js";
+import "./Avatar-275b4fa0.js";
+import "./Badge-b72f2d39.js";
+import "./FeatherIcon-9c46386e.js";
+import "./Checkbox-185d8ed3.js";
+import "./Autocomplete-adb07502.js";
+import "./Popover-31ec707d.js";
+import "./Breadcrumbs-4eccad0a.js";
+import "./Dropdown-b9370ca1.js";
+import "./DateRangePicker-f940f2a4.js";
+import "./TextInput-efbd944a.js";
+import "./debounce-d11286cd.js";
+import "./ErrorMessage-b567f058.js";
+import "./FileUploader-59526497.js";
+import "./FormControl-d05dcba9.js";
+import "./Select-6c682405.js";
+import "./Textarea-75fa0e2a.js";
+import "./Progress-cc7f81b9.js";
+import "./Rating-cd50fa1c.js";
+import "./Spinner-09477bc4.js";
+import "./Switch-95beb185.js";
+import "./Calendar-6e1dfa04.js";
+import "./Tabs-b7d3ba38.js";
+import "./Tooltip-f232eaa6.js";
+import "./CircularProgressBar-7c4f26a7.js";
+import "./Tree-c8b87f99.js";
+const _sfc_main = {
+  name: "InsertImage",
+  props: ["editor"],
+  expose: ["openDialog"],
+  data() {
+    return {
+      addImageDialog: { url: "", file: null, show: false }
+    };
+  },
+  components: { Button, Dialog },
+  methods: {
+    openDialog() {
+      this.addImageDialog.show = true;
+    },
+    onImageSelect(e) {
+      let file = e.target.files[0];
+      if (!file) {
+        return;
+      }
+      this.addImageDialog.file = file;
+      fileToBase64(file).then((base64) => {
+        this.addImageDialog.url = base64;
+      });
+    },
+    addImage(src) {
+      this.editor.chain().focus().setImage({ src }).run();
+      this.reset();
+    },
+    reset() {
+      this.addImageDialog = this.$options.data().addImageDialog;
+    }
+  }
+};
+const _hoisted_1 = { class: "relative cursor-pointer rounded-lg bg-gray-100 py-1 focus-within:bg-gray-200 hover:bg-gray-200" };
+const _hoisted_2 = { class: "absolute inset-0 select-none px-2 py-1 text-base" };
+const _hoisted_3 = ["src"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Button = resolveComponent("Button");
+  const _component_Dialog = resolveComponent("Dialog");
+  return openBlock(), createElementBlock(
+    Fragment,
+    null,
+    [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: $options.openDialog }))),
+      createVNode(_component_Dialog, {
+        options: { title: "Add Image" },
+        modelValue: $data.addImageDialog.show,
+        "onUpdate:modelValue": _cache[2] || (_cache[2] = ($event) => $data.addImageDialog.show = $event),
+        onAfterLeave: $options.reset
+      }, {
+        "body-content": withCtx(() => [
+          createBaseVNode("label", _hoisted_1, [
+            createBaseVNode(
+              "input",
+              {
+                type: "file",
+                class: "w-full opacity-0",
+                onChange: _cache[0] || (_cache[0] = (...args) => $options.onImageSelect && $options.onImageSelect(...args)),
+                accept: "image/*"
+              },
+              null,
+              32
+              /* NEED_HYDRATION */
+            ),
+            createBaseVNode(
+              "span",
+              _hoisted_2,
+              toDisplayString($data.addImageDialog.file ? "Select another image" : "Select an image"),
+              1
+              /* TEXT */
+            )
+          ]),
+          $data.addImageDialog.url ? (openBlock(), createElementBlock("img", {
+            key: 0,
+            src: $data.addImageDialog.url,
+            class: "mt-2 w-full rounded-lg"
+          }, null, 8, _hoisted_3)) : createCommentVNode("v-if", true)
+        ]),
+        actions: withCtx(() => [
+          createVNode(_component_Button, {
+            variant: "solid",
+            onClick: _cache[1] || (_cache[1] = ($event) => $options.addImage($data.addImageDialog.url))
+          }, {
+            default: withCtx(() => _cache[3] || (_cache[3] = [
+              createTextVNode(" Insert Image ")
+            ])),
+            _: 1
+            /* STABLE */
+          }),
+          createVNode(_component_Button, { onClick: $options.reset }, {
+            default: withCtx(() => _cache[4] || (_cache[4] = [
+              createTextVNode(" Cancel ")
+            ])),
+            _: 1
+            /* STABLE */
+          }, 8, ["onClick"])
+        ]),
+        _: 1
+        /* STABLE */
+      }, 8, ["modelValue", "onAfterLeave"])
+    ],
+    64
+    /* STABLE_FRAGMENT */
+  );
+}
+_sfc_main.__file = "src/components/TextEditor/InsertImage.vue";
+const InsertImage = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/InsertImage.vue"]]);
+export {
+  InsertImage as default
+};
---
+++ assets/Spinner-09477bc4.js
@@ -0,0 +1,56 @@
+import { aB as openBlock, aG as createElementBlock, aF as createBaseVNode } from "./vendor-ff760363.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-cc2b3d55.js";
+const Spinner_vue_vue_type_style_index_0_scoped_259be2b2_lang = "";
+const _sfc_main = {};
+const _hoisted_1 = {
+  class: "spinner",
+  viewBox: "0 0 50 50"
+};
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "defs",
+      null,
+      [
+        createBaseVNode("linearGradient", {
+          id: "gradient",
+          x1: "0%",
+          y1: "0%",
+          x2: "0%",
+          y2: "100%"
+        }, [
+          createBaseVNode("stop", {
+            offset: "0%",
+            "stop-color": "rgba(0,110,219,1)"
+          }),
+          createBaseVNode("stop", {
+            offset: "100%",
+            "stop-color": "rgba(255,255,255,0)"
+          })
+        ])
+      ],
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "circle",
+      {
+        stroke: "url(#gradient)",
+        class: "spinner-path",
+        cx: "25",
+        cy: "25",
+        r: "20",
+        fill: "none",
+        "stroke-width": "5"
+      },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+_sfc_main.__file = "src/components/Spinner.vue";
+const Spinner = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-259be2b2"], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/Spinner.vue"]]);
+export {
+  Spinner as S
+};
