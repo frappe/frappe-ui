845f056fd2d324ae597883e4aef6599f61f043f7
Faris Ansari <netchamp.faris@gmail.com>
2024-11-16 18:50:55 +0530

    fix: better dark mode histoire setup
    - works in dev and prod

--- histoire.setup.ts
+++ histoire.setup.ts
@@ -1,22 +1,35 @@
 import './histoire.css'
 import './src/style.css'
 
-// TODO: Fix a better way to handle this
-// Hack
+// development
+if (document.readyState == 'complete') {
+  updateThemeAttrOnThemeChange()
+}
+
+// production
 window.addEventListener('DOMContentLoaded', () => {
-  document
-    .querySelector(
-      '#app > div > div > div.htw-relative.htw-top-0.htw-left-0.htw-z-20.htw-border-r.htw-border-gray-300\\/30.dark\\:htw-border-gray-800 > div.htw-flex.htw-flex-col.htw-h-full.htw-bg-gray-100.dark\\:htw-bg-gray-750.__histoire-pane-shadow-from-right > div.histoire-app-header.htw-px-4.htw-h-16.htw-flex.htw-items-center.htw-gap-2.htw-flex-none > div.htw-ml-auto.htw-flex-none.htw-flex > a:nth-child(2)',
-    )
-    ?.addEventListener('click', () => {
-      console.log('clicked')
-      if (document.documentElement.classList.contains('htw-dark')) {
+  updateThemeAttrOnThemeChange()
+})
+
+function updateThemeAttrOnThemeChange() {
+  let observer = new MutationObserver((mutations) => {
+    for (const m of mutations) {
+      const newValue = m.target.getAttribute(m.attributeName)
+
+      if (newValue === 'htw-dark') {
         document.documentElement.setAttribute('data-theme', 'dark')
       } else {
         document.documentElement.setAttribute('data-theme', 'light')
       }
-    })
-})
+    }
+  })
+  // observe changes to the class attribute on root element
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ['class'],
+  })
+}
 
 // handle route param in url
 const urlParams = new URLSearchParams(window.location.search)
