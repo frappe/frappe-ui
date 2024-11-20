0a8a716277b7d2bdc1de25063cd22b66dcc9b502
netchampfaris <netchampfaris@users.noreply.github.com>
2024-11-20 10:14:45 +0000

    deploy: a7dffe63f2fd919478b5daea6c7550fba5dd5bd1

---
+++ assets/InsertImage-752f0b0a.js
@@ -0,0 +1,140 @@
+import { f as fileToBase64 } from "./ListView.story-b5be7d7c.js";
+import { D as Dialog } from "./Dialog-7f9924a7.js";
+import { B as Button } from "./Button-1d668538.js";
+import { aA as resolveComponent, aB as openBlock, aG as createElementBlock, aT as renderSlot, aU as normalizeProps, aV as guardReactiveProps, aE as createVNode, aD as withCtx, aF as createBaseVNode, aN as toDisplayString, aH as createCommentVNode, aM as createTextVNode, aK as Fragment } from "./vendor-7bda5848.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-cc2b3d55.js";
+import "./Avatar-3a8c1fae.js";
+import "./Badge-bb48a2f0.js";
+import "./FeatherIcon-27cb7a2f.js";
+import "./Checkbox-10522f69.js";
+import "./Autocomplete-80623979.js";
+import "./Popover-c85e7be0.js";
+import "./Breadcrumbs-6a1b23ce.js";
+import "./Dropdown-7dbdf326.js";
+import "./DateRangePicker-03172fcd.js";
+import "./TextInput-e206d27f.js";
+import "./debounce-d11286cd.js";
+import "./ErrorMessage-8704d32d.js";
+import "./FileUploader-5df7a811.js";
+import "./FormControl-c772891c.js";
+import "./Select-fbed7463.js";
+import "./Textarea-3c5ab3f1.js";
+import "./Progress-a5ea8100.js";
+import "./Rating-0c5f9971.js";
+import "./Spinner-f375d23e.js";
+import "./Switch-d5215497.js";
+import "./Calendar-e9c1b1fb.js";
+import "./Tabs-73e30005.js";
+import "./Tooltip-cfb64f5e.js";
+import "./CircularProgressBar-e2a57b1c.js";
+import "./Tree-c827cb10.js";
+const _sfc_main = {
+  name: "InsertImage",
+  props: ["editor"],
+  expose: ["openDialog"],
+  data() {
+    return {
+      addImageDialog: { url: "", file: null, show: false }
+    };
+  },
+  components: { Button, Dialog },
+  methods: {
+    openDialog() {
+      this.addImageDialog.show = true;
+    },
+    onImageSelect(e) {
+      let file = e.target.files[0];
+      if (!file) {
+        return;
+      }
+      this.addImageDialog.file = file;
+      fileToBase64(file).then((base64) => {
+        this.addImageDialog.url = base64;
+      });
+    },
+    addImage(src) {
+      this.editor.chain().focus().setImage({ src }).run();
+      this.reset();
+    },
+    reset() {
+      this.addImageDialog = this.$options.data().addImageDialog;
+    }
+  }
+};
+const _hoisted_1 = { class: "relative cursor-pointer rounded-lg bg-gray-100 py-1 focus-within:bg-gray-200 hover:bg-gray-200" };
+const _hoisted_2 = { class: "absolute inset-0 select-none px-2 py-1 text-base" };
+const _hoisted_3 = ["src"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Button = resolveComponent("Button");
+  const _component_Dialog = resolveComponent("Dialog");
+  return openBlock(), createElementBlock(
+    Fragment,
+    null,
+    [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: $options.openDialog }))),
+      createVNode(_component_Dialog, {
+        options: { title: "Add Image" },
+        modelValue: $data.addImageDialog.show,
+        "onUpdate:modelValue": _cache[2] || (_cache[2] = ($event) => $data.addImageDialog.show = $event),
+        onAfterLeave: $options.reset
+      }, {
+        "body-content": withCtx(() => [
+          createBaseVNode("label", _hoisted_1, [
+            createBaseVNode(
+              "input",
+              {
+                type: "file",
+                class: "w-full opacity-0",
+                onChange: _cache[0] || (_cache[0] = (...args) => $options.onImageSelect && $options.onImageSelect(...args)),
+                accept: "image/*"
+              },
+              null,
+              32
+              /* NEED_HYDRATION */
+            ),
+            createBaseVNode(
+              "span",
+              _hoisted_2,
+              toDisplayString($data.addImageDialog.file ? "Select another image" : "Select an image"),
+              1
+              /* TEXT */
+            )
+          ]),
+          $data.addImageDialog.url ? (openBlock(), createElementBlock("img", {
+            key: 0,
+            src: $data.addImageDialog.url,
+            class: "mt-2 w-full rounded-lg"
+          }, null, 8, _hoisted_3)) : createCommentVNode("v-if", true)
+        ]),
+        actions: withCtx(() => [
+          createVNode(_component_Button, {
+            variant: "solid",
+            onClick: _cache[1] || (_cache[1] = ($event) => $options.addImage($data.addImageDialog.url))
+          }, {
+            default: withCtx(() => _cache[3] || (_cache[3] = [
+              createTextVNode(" Insert Image ")
+            ])),
+            _: 1
+            /* STABLE */
+          }),
+          createVNode(_component_Button, { onClick: $options.reset }, {
+            default: withCtx(() => _cache[4] || (_cache[4] = [
+              createTextVNode(" Cancel ")
+            ])),
+            _: 1
+            /* STABLE */
+          }, 8, ["onClick"])
+        ]),
+        _: 1
+        /* STABLE */
+      }, 8, ["modelValue", "onAfterLeave"])
+    ],
+    64
+    /* STABLE_FRAGMENT */
+  );
+}
+_sfc_main.__file = "src/components/TextEditor/InsertImage.vue";
+const InsertImage = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/InsertImage.vue"]]);
+export {
+  InsertImage as default
+};
