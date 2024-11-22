03168d558957fcdc41c3576218ff6a01bf223daa
bhargavleremitt <bhargav.m@leremitt.com>
2024-11-22 16:20:00 +0530

    Update Breadcrumbs.vue

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -33,12 +33,7 @@
       <template v-for="(item, i) in crumbs" :key="item.label">
         <component
           :is="item.route ? 'router-link' : 'button'"
-          class="flex items-center rounded px-0.5 py-1 text-lg font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-400"
-          :class="[
-            i == crumbs.length - 1
-              ? 'text-gray-900'
-              : 'text-gray-600 hover:text-gray-700',
-          ]"
+          class="flex items-center rounded px-1 py-1 text-lg font-medium text-white hover:bg-[#007be0]"
           v-bind="item.route ? { to: item.route } : { onClick: item.onClick }"
         >
           <slot name="prefix" :item="item" />
@@ -49,7 +44,7 @@
         </component>
         <span
           v-if="i != crumbs.length - 1"
-          class="mx-0.5 text-base text-gray-500"
+          class="mx-0.5 text-base text-white"
           aria-hidden="true"
         >
           /
