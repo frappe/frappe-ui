13ade1b61d93d4b8b16355b7a62cf8789f39774e
netchampfaris <netchampfaris@users.noreply.github.com>
2024-11-25 20:57:35 +0000

    deploy: 82ce2cf2bad68043d4cc76a9acd98d8fc1d6fe5c

---
+++ assets/FontColor-88090096.js
@@ -0,0 +1,173 @@
+import { P as Popover } from "./Popover-62cb000a.js";
+import "./ListView.story-99b5dcaa.js";
+import "./Autocomplete-01d57186.js";
+import "./Avatar-944e8f98.js";
+import "./Badge-f614bce1.js";
+import "./Breadcrumbs-85b9df09.js";
+import "./Button-d49fd6e1.js";
+import "./Checkbox-f8711e8e.js";
+import "./DateRangePicker-4f280a4a.js";
+import "./Dialog-331d781e.js";
+import "./Dropdown-83a1d8be.js";
+import "./ErrorMessage-77d80957.js";
+import "./FeatherIcon-338b7dab.js";
+import "./FileUploader-0bb4e9b7.js";
+import "./FormControl-5fa64679.js";
+import "./Progress-42457c8d.js";
+import "./Rating-25a552fb.js";
+import "./Select-28ebb977.js";
+import "./Spinner-cf572fa0.js";
+import "./Switch-8e9e4e8c.js";
+import "./TabButtons-fed9848f.js";
+import "./Tabs-e872c7dd.js";
+import "./TextInput-4fb80d02.js";
+import "./Textarea-d6e62e42.js";
+import { T as Tooltip } from "./Tooltip-c822e04a.js";
+import "./Calendar-658e7fdf.js";
+import "./CircularProgressBar-56696b25.js";
+import "./Tree-889fd7b7.js";
+import { aA as resolveComponent, aB as openBlock, aC as createBlock, aD as withCtx, aT as renderSlot, aU as normalizeProps, aV as guardReactiveProps, aF as createBaseVNode, aG as createElementBlock, aK as Fragment, aJ as renderList, aY as normalizeStyle, aW as normalizeClass } from "./vendor-2faef316.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-cc2b3d55.js";
+import "./debounce-d11286cd.js";
+const _sfc_main = {
+  name: "FontColor",
+  props: ["editor"],
+  components: { Popover, Tooltip },
+  methods: {
+    setBackgroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().toggleHighlight({ color: color.hex }).run();
+      } else {
+        this.editor.chain().focus().unsetHighlight().run();
+      }
+    },
+    setForegroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().setColor(color.hex).run();
+      } else {
+        this.editor.chain().focus().unsetColor().run();
+      }
+    }
+  },
+  computed: {
+    foregroundColors() {
+      return [
+        { name: "Default", hex: "#1F272E" },
+        { name: "Yellow", hex: "#ca8a04" },
+        { name: "Orange", hex: "#ea580c" },
+        { name: "Red", hex: "#dc2626" },
+        { name: "Green", hex: "#16a34a" },
+        { name: "Blue", hex: "#1579D0" },
+        { name: "Purple", hex: "#9333ea" },
+        { name: "Pink", hex: "#db2777" }
+      ];
+    },
+    backgroundColors() {
+      return [
+        { name: "Default", hex: null },
+        { name: "Yellow", hex: "#fef9c3" },
+        { name: "Orange", hex: "#ffedd5" },
+        { name: "Red", hex: "#fee2e2" },
+        { name: "Green", hex: "#dcfce7" },
+        { name: "Blue", hex: "#D3E9FC" },
+        { name: "Purple", hex: "#f3e8ff" },
+        { name: "Pink", hex: "#fce7f3" }
+      ];
+    }
+  }
+};
+const _hoisted_1 = { class: "p-2" };
+const _hoisted_2 = { class: "mt-1 grid grid-cols-8 gap-1" };
+const _hoisted_3 = ["aria-label", "onClick"];
+const _hoisted_4 = { class: "mt-1 grid grid-cols-8 gap-1" };
+const _hoisted_5 = ["aria-label", "onClick"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Tooltip = resolveComponent("Tooltip");
+  const _component_Popover = resolveComponent("Popover");
+  return openBlock(), createBlock(_component_Popover, { transition: "default" }, {
+    target: withCtx(({ togglePopover, isOpen }) => [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: () => togglePopover(), isActive: isOpen })))
+    ]),
+    "body-main": withCtx(() => [
+      createBaseVNode("div", _hoisted_1, [
+        _cache[0] || (_cache[0] = createBaseVNode(
+          "div",
+          { class: "text-sm text-ink-gray-7" },
+          "Text Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_2, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.foregroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: "flex h-5 w-5 items-center justify-center rounded border text-base",
+                    style: normalizeStyle({
+                      color: color.hex
+                    }),
+                    onClick: ($event) => $options.setForegroundColor(color)
+                  }, " A ", 12, _hoisted_3)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ]),
+        _cache[1] || (_cache[1] = createBaseVNode(
+          "div",
+          { class: "mt-2 text-sm text-ink-gray-7" },
+          "Background Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_4, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.backgroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: normalizeClass(["flex h-5 w-5 items-center justify-center rounded border text-base text-ink-gray-9", !color.hex ? "border-outline-gray-modals" : "border-transparent"]),
+                    style: normalizeStyle({
+                      backgroundColor: color.hex
+                    }),
+                    onClick: ($event) => $options.setBackgroundColor(color)
+                  }, " A ", 14, _hoisted_5)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ])
+      ])
+    ]),
+    _: 3
+    /* FORWARDED */
+  });
+}
+_sfc_main.__file = "src/components/TextEditor/FontColor.vue";
+const FontColor = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/FontColor.vue"]]);
+export {
+  FontColor as default
+};
