d071fecced7aeb43b8b18de2fad0301e7fa01d75
netchampfaris <netchampfaris@users.noreply.github.com>
2024-11-25 20:05:52 +0000

    deploy: 1a4e4af5672af13b8d5eac5827a201ec3b11cfa6

---
+++ assets/Breadcrumbs.story-941cb607.js
@@ -0,0 +1,111 @@
+import { ay as defineComponent, aA as resolveComponent, aB as openBlock, aC as createBlock, aD as withCtx, aO as logEvent, aE as createVNode, aF as createBaseVNode, aN as toDisplayString } from "./vendor-0c74789c.js";
+import { B as Breadcrumbs } from "./Breadcrumbs-64c09f9a.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-cc2b3d55.js";
+import "./Dropdown-b8373d76.js";
+import "./Popover-d6a5b56d.js";
+import "./Button-01c4005e.js";
+import "./FeatherIcon-9e3c9435.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "Breadcrumbs.story",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const __returned__ = { get logEvent() {
+      return logEvent;
+    }, Breadcrumbs };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1 = { class: "mr-1" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Variant = resolveComponent("Variant");
+  const _component_Story = resolveComponent("Story");
+  return openBlock(), createBlock(_component_Story, { layout: { type: "grid", width: 500 } }, {
+    default: withCtx(() => [
+      createVNode(_component_Variant, { title: "With route option" }, {
+        default: withCtx(() => [
+          createVNode($setup["Breadcrumbs"], { items: [
+            {
+              label: "Home",
+              route: { name: "Home" }
+            },
+            {
+              label: "Views",
+              route: "/components"
+            },
+            {
+              label: "List",
+              route: "/components/breadcrumbs"
+            }
+          ] })
+        ]),
+        _: 1
+        /* STABLE */
+      }),
+      createVNode(_component_Variant, { title: "With onClick option" }, {
+        default: withCtx(() => [
+          createVNode($setup["Breadcrumbs"], {
+            items: [
+              {
+                label: "Home",
+                onClick: () => $setup.logEvent("onClick", "Home")
+              },
+              {
+                label: "Views",
+                onClick: () => $setup.logEvent("onClick", "Home")
+              },
+              {
+                label: "Kanban",
+                onClick: () => $setup.logEvent("onClick", "Home")
+              }
+            ]
+          }, null, 8, ["items"])
+        ]),
+        _: 1
+        /* STABLE */
+      }),
+      createVNode(_component_Variant, { title: "With prefix slot" }, {
+        default: withCtx(() => [
+          createVNode($setup["Breadcrumbs"], { items: [
+            {
+              label: "Home",
+              icon: "🏡",
+              route: { name: "Home" }
+            },
+            {
+              label: "Views",
+              icon: "🏞️",
+              route: "/components"
+            },
+            {
+              label: "List",
+              icon: "📃",
+              route: "/components/breadcrumbs"
+            }
+          ] }, {
+            prefix: withCtx(({ item }) => [
+              createBaseVNode(
+                "span",
+                _hoisted_1,
+                toDisplayString(item.icon),
+                1
+                /* TEXT */
+              )
+            ]),
+            _: 1
+            /* STABLE */
+          })
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 1
+    /* STABLE */
+  });
+}
+_sfc_main.__file = "src/components/Breadcrumbs.story.vue";
+const Breadcrumbs_story = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/Breadcrumbs.story.vue"]]);
+export {
+  Breadcrumbs_story as default
+};
