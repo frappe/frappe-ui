1cbef8bfb16c6e7845238d2817a0add4b3b2b2f7
Faris Ansari <netchamp.faris@gmail.com>
2024-11-28 13:48:30 +0530

    fix: adjust brightness/contrast for images in dark mode

--- src/tailwind/plugin.js
+++ src/tailwind/plugin.js
@@ -44,6 +44,9 @@ let componentStyles = {
   "[data-theme='dark'] [type='checkbox']:checked": {
     'background-image': `url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='%230F0F0F' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z'/%3e%3c/svg%3e")`,
   },
+  "[data-theme='dark'] img": {
+    filter: 'brightness(.8) contrast(1.2)',
+  },
 }
 
 module.exports = plugin(
