0068c3b40a21ce09780e550efcbcee8edc9bd40a
Faris Ansari <netchamp.faris@gmail.com>
2024-12-18 16:51:29 +0530

    fix: detect inline markdown content
    instead of just block markdown

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -15,6 +15,15 @@ export function detectMarkdown(text) {
   const lines = text.split('\n')
   const markdown = lines.filter(
     (line) =>
+      // check for inline markdown content like images, links, italic, bold, etc.
+      /!\[.*\]\(.*\)/.test(line) ||
+      /\[.*\]\(.*\)/.test(line) ||
+      /(^|\s)\*.*\*(\s|$)/.test(line) ||
+      /(^|\s)_.*_(\s|$)/.test(line) ||
+      /(^|\s)\*\*.*\*\*(\s|$)/.test(line) ||
+      /(^|\s)__.*__(\s|$)/.test(line) ||
+      /(^|\s)~~.*~~(\s|$)/.test(line) ||
+      // check for block markdown content like headings, code blocks, lists, etc.
       line.startsWith('![') ||
       line.startsWith('#') ||
       line.startsWith('> ') ||
