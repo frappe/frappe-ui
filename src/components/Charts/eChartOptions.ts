5b4a63d7924af5ecee988c86ce5ce93d200c37ef
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-03-15 17:08:55 +0530

    feat: add `BarChart` component

---
+++ src/components/Charts/eChartOptions.ts
@@ -0,0 +1,308 @@
+import { formatDate, formatLabel, formatValue } from './helpers'
+import { AxisChartConfig } from './types'
+
+const PADDING_TOP = 0
+const PADDING_BOTTOM = 10
+const AXIS_TITLE_HEIGHT = 20
+const LEGEND_HEIGHT = 30
+const LEGEND_BOTTOM = 10
+const TITLE_HEIGHT = 40
+const SUBTITLE_HEIGHT = 30
+
+export default function useEchartsOptions(config: AxisChartConfig) {
+  const title = config.title
+  const subtitle = config.subtitle
+  const hasTitle = title ? 1 : 0
+  const hasSubtitle = subtitle ? 1 : 0
+  const hasXAxisTitle = config.xAxisTitle ? 1 : 0
+  const hasYAxisTitle = config.yAxisTitle ? 1 : 0
+  const hasY2AxisTitle = config.y2AxisTitle ? 1 : 0
+  const hasLegend = config.series.length > 1 ? 1 : 0
+
+  const xAxisOptions = getXAxisOptions(config)
+  const yAxisOptions = getYAxisOptions(config)
+
+  return {
+    textStyle: {
+      fontFamily: ['InterVar', 'sans-serif'],
+    },
+    title: {
+      top: '4px',
+      left: '0.8%',
+      text: title,
+      subtext: subtitle,
+      padding: 0,
+      itemGap: -3,
+      textStyle: {
+        fontSize: 14,
+        fontWeight: 500,
+        lineHeight: 24,
+        // color: titleColor
+      },
+      subtextStyle: {
+        fontSize: 13,
+        fontWeight: 400,
+        lineHeight: 20,
+        // color: subtitleColor,
+      },
+    },
+    animation: true,
+    animationDuration: 700,
+    grid: {
+      left: '1%',
+      right: config.swapAxes ? '2.5%' : '1%',
+      top:
+        PADDING_TOP + TITLE_HEIGHT * hasTitle + SUBTITLE_HEIGHT * hasSubtitle,
+      bottom: PADDING_BOTTOM + LEGEND_HEIGHT * hasLegend,
+      containLabel: true,
+    },
+    color: config.colors,
+    xAxis: xAxisOptions,
+    yAxis: yAxisOptions,
+    series: [],
+    tooltip: {
+      show: true,
+      trigger: 'axis',
+      formatter: (params: Object | Array<Object>) => {
+        if (Array.isArray(params)) {
+          params = params
+            .filter((p) => p.value?.[1] !== 0)
+            .sort((a, b) => b.value?.[1] - a.value?.[1])
+        }
+
+        if (!Array.isArray(params)) {
+          const p = params as any
+          const value = config.swapAxes ? p.value[0] : p.value[1]
+          const formatted = isNaN(value) ? value : formatValue(value)
+          return `
+								<div class="flex items-center justify-between gap-5">
+									<div>${p.name}</div>
+									<div class="font-bold">${formatted}</div>
+								</div>
+							`
+        }
+        if (Array.isArray(params)) {
+          const t = params.map((p, idx) => {
+            const xValue = config.swapAxes ? p.value[1] : p.value[0]
+            const yValue = config.swapAxes ? p.value[0] : p.value[1]
+            const formattedX =
+              config.xType == 'time' && config.timeGrain
+                ? formatDate(xValue, undefined, config.timeGrain)
+                : xValue
+            const formattedY = isNaN(yValue) ? yValue : formatValue(yValue)
+            return `
+							<div class="flex flex-col">
+								${idx == 0 ? `<div>${formattedX}</div>` : ''}
+								<div class="flex items-center justify-between gap-5">
+									<div class="flex gap-1 items-center">
+										${p.marker}
+										<div>${formatLabel(p.seriesName)}:</div>
+									</div>
+									<div class="font-bold">${formattedY}</div>
+								</div>
+							</div>
+						`
+          })
+          return t.join('')
+        }
+      },
+      confine: true,
+      appendToBody: false,
+      axisPointer: {
+        type: 'shadow',
+      },
+      order: 'valueDesc',
+    },
+    legend: {
+      show: hasLegend,
+      type: 'scroll',
+      bottom: LEGEND_BOTTOM,
+      orient: 'horizontal',
+      itemGap: 12,
+      padding: [0, 25],
+      formatter: function (name: string) {
+        return formatLabel(name)
+      },
+      textStyle: {
+        padding: [0, 0, 0, -5],
+        color: '#000',
+      },
+      icon: 'circle',
+      pageIcons: {
+        horizontal: [
+          'M 17 3 h 2 c 0.386 0 0.738 0.223 0.904 0.572 s 0.115 0.762 -0.13 1.062 L 11.292 15 l 8.482 10.367 c 0.245 0.299 0.295 0.712 0.13 1.062 S 19.386 27 19 27 h -2 c -0.3 0 -0.584 -0.135 -0.774 -0.367 l -9 -11 c -0.301 -0.369 -0.301 -0.898 0 -1.267 l 9 -11 C 16.416 3.135 16.7 3 17 3 Z',
+          'M 12 27 h -2 c -0.386 0 -0.738 -0.223 -0.904 -0.572 s -0.115 -0.762 0.13 -1.062 L 17.708 15 L 9.226 4.633 c -0.245 -0.299 -0.295 -0.712 -0.13 -1.062 S 9.614 3 10 3 h 2 c 0.3 0 0.584 0.135 0.774 0.367 l 9 11 c 0.301 0.369 0.301 0.898 0 1.267 l -9 11 C 12.584 26.865 12.3 27 12 27 Z',
+        ],
+      },
+      pageIconColor: '#64748B',
+      pageInactiveColor: '#C0CCDA',
+      pageIconSize: 10,
+      pageTextStyle: {
+        color: '#64748B',
+      },
+      animationDurationUpdate: 300,
+    },
+  }
+}
+
+function getXAxisOptions(config: AxisChartConfig) {
+  return config.swapAxes
+    ? {
+        type: 'value',
+        scale: false,
+        z: 2,
+        alignTicks: true,
+        boundaryGap: false,
+        axisLabel: {
+          show: true,
+          hideOverlap: true,
+          margin: 8,
+          formatter: function (value: number) {
+            return formatValue(value, 1, true)
+          },
+        },
+        splitLine: {
+          show: true,
+          width: 1,
+        },
+        axisLine: {
+          show: false,
+          onZero: false,
+        },
+        axisTick: {
+          show: false,
+          alignWithLabel: true,
+          formatter: function (value: number) {
+            return formatValue(value, 1, true)
+          },
+        },
+      }
+    : {
+        z: 2,
+        type: config.xType,
+        scale: true,
+        splitLine: {
+          show: false,
+        },
+        axisLine: {
+          show: true,
+        },
+        axisTick: {
+          show: false,
+          alignWithLabel: true,
+        },
+        axisLabel: {
+          show: true,
+          hideOverlap: true,
+          showMaxLabel: config.xType === 'category' || config.xType === 'value',
+          margin: 6,
+        },
+      }
+}
+
+function getYAxisOptions(config: AxisChartConfig) {
+  const primaryYAxisOptions = config.swapAxes
+    ? {
+        show: true,
+        type: config.xType,
+        z: 2,
+        scale: true,
+        inverse: 'true',
+        splitLine: {
+          show: false,
+          width: 1,
+        },
+        axisLine: {
+          show: true,
+        },
+        axisTick: {
+          show: false,
+          alignWithLabel: true,
+        },
+        axisLabel: {
+          show: true,
+          hideOverlap: true,
+        },
+      }
+    : {
+        show: true,
+        type: 'value',
+        z: 2,
+        scale: false,
+        boundaryGap: ['0%', '1%'],
+        name: `↑ ${config.yAxisTitle}`,
+        nameLocation: 'end',
+        nameTextStyle: {
+          align: 'left',
+          verticalAlign: 'top',
+          padding: [0, 0, 0, -2],
+          // color: '#000',
+          backgroundColor: '#fff',
+          borderColor: '#fff',
+          borderWidth: 4,
+        },
+        nameGap: 6,
+        splitLine: {
+          show: true,
+          width: 1,
+        },
+        axisLine: {
+          show: false,
+          onZero: false,
+        },
+        axisTick: {
+          show: false,
+          alignWithLabel: true,
+        },
+        axisLabel: {
+          show: true,
+          hideOverlap: true,
+          margin: 8,
+          formatter: function (value: number) {
+            return formatValue(value, 1, true)
+          },
+        },
+      }
+
+  const secondaryYAxisOptions = {
+    show: false,
+    type: 'value',
+    z: 2,
+    alignTicks: true,
+    scale: false,
+    boundaryGap: ['0%', '1%'],
+    name: `${config.y2AxisTitle} ↑`,
+    nameLocation: 'end',
+    nameTextStyle: {
+      align: 'right',
+      verticalAlign: 'top',
+      padding: [0, 5, 0, 0],
+      // color: '#000',
+      backgroundColor: '#fff',
+    },
+    nameGap: 6,
+    splitLine: {
+      show: true,
+      width: 1,
+    },
+    axisLine: {
+      show: false,
+      onZero: false,
+    },
+    axisTick: {
+      show: false,
+      alignWithLabel: true,
+    },
+    axisLabel: {
+      show: true,
+      hideOverlap: true,
+      margin: 8,
+      formatter: function (value: number) {
+        return formatValue(value, 1, true)
+      },
+      // color: '#000',
+    },
+  }
+
+  return [primaryYAxisOptions, secondaryYAxisOptions]
+}
