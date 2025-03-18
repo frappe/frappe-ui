178c0a76ad3fa8bbff60e52aba04de9602c11fae
Shariq Ansari <sharique.rik@gmail.com>
2025-03-19 01:56:16 +0530

    fix: handle banners for dark mode

--- src/frappe/components/GettingStartedBanner.vue
+++ src/frappe/components/GettingStartedBanner.vue
@@ -1,12 +1,15 @@
 <template>
   <div
     v-if="!isSidebarCollapsed"
-    class="m-2 flex flex-col gap-3 shadow-sm rounded-lg py-2.5 px-3 bg-surface-white text-base"
+    class="m-2 flex flex-col gap-3 shadow-sm rounded-lg py-2.5 px-3 bg-surface-modal text-base"
   >
-    <div v-if="stepsCompleted != totalSteps" class="inline-flex gap-2">
+    <div
+      v-if="stepsCompleted != totalSteps"
+      class="inline-flex text-ink-gray-9 gap-2"
+    >
       <StepsIcon class="h-4 my-0.5 shrink-0" />
       <div class="flex flex-col text-p-sm gap-0.5">
-        <div class="text-ink-gray-9 font-medium">
+        <div class="font-medium">
           {{ 'Gettings started' }}
         </div>
         <div class="text-ink-gray-7">
