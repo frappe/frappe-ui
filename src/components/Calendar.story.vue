eba0f00ae4896437598e96e435b8c0e86f146b11
Shariq Ansari <sharique.rik@gmail.com>
2025-04-03 12:09:52 +0530

    fix: load events dates dynamically

--- src/components/Calendar.story.vue
+++ src/components/Calendar.story.vue
@@ -5,9 +5,9 @@
         <Calendar
           :config="config"
           :events="events"
-          :create="(event) => logEvent('createEvent', event)"
-          :update="(event) => logEvent('updateEvent', event)"
-          :delete="(event) => logEvent('deleteEvent', event)"
+          @create="(event) => logEvent('createEvent', event)"
+          @update="(event) => logEvent('updateEvent', event)"
+          @delete="(event) => logEvent('deleteEvent', event)"
         >
         </Calendar>
       </div>
@@ -17,9 +17,9 @@
         <Calendar
           :config="config"
           :events="events"
-          :create="(event) => logEvent('createEvent', event)"
-          :update="(event) => logEvent('updateEvent', event)"
-          :delete="(event) => logEvent('deleteEvent', event)"
+          @create="(event) => logEvent('createEvent', event)"
+          @update="(event) => logEvent('updateEvent', event)"
+          @delete="(event) => logEvent('deleteEvent', event)"
         >
           <template
             #header="{
@@ -76,14 +76,24 @@ const config = {
   enableShortcuts: false,
 }
 
+function getCurrentMonthYear() {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+
+  return `${year}-${month}`
+}
+
+const currentMonthYear = getCurrentMonthYear()
+
 const events = ref([
   {
     title: 'English by Ryan Mathew',
     participant: 'Ryan Mathew',
     id: 'EDU-CSH-2024-00091',
     venue: 'CNF-ROOM-2024-00001',
-    fromDate: '2024-10-08 16:30:00', //can be a date object
-    toDate: '2024-10-08 17:30:00',
+    fromDate: currentMonthYear + '-02 16:30:00', //can be a date object
+    toDate: currentMonthYear + '-02 17:30:00',
     color: 'violet',
   },
   {
@@ -91,8 +101,8 @@ const events = ref([
     participant: 'Ryan Mathew',
     id: 'EDU-CSH-2024-00092',
     venue: 'CNF-ROOM-2024-00002',
-    fromDate: '2024-10-08 13:30:00',
-    toDate: '2024-10-08 17:30:00',
+    fromDate: currentMonthYear + '-04 13:30:00',
+    toDate: currentMonthYear + '-04 17:30:00',
     color: 'green',
   },
   {
@@ -100,8 +110,8 @@ const events = ref([
     participant: 'Sheldon',
     id: 'EDU-CSH-2024-00093',
     venue: 'CNF-ROOM-2024-00001',
-    fromDate: '2024-10-09 10:30:00',
-    toDate: '2024-10-09 11:30:00',
+    fromDate: currentMonthYear + '-16 10:30:00',
+    toDate: currentMonthYear + '-16 11:30:00',
     color: 'blue',
   },
   {
@@ -109,8 +119,8 @@ const events = ref([
     participant: 'Ryan Mathew',
     id: 'EDU-CSH-2024-00094',
     venue: 'CNF-ROOM-2024-00001',
-    fromDate: '2024-10-17 16:30:00',
-    toDate: '2024-10-17 17:30:00',
+    fromDate: currentMonthYear + '-21 16:30:00',
+    toDate: currentMonthYear + '-21 17:30:00',
     color: 'red',
   },
   {
@@ -118,8 +128,8 @@ const events = ref([
     participant: 'John',
     id: '#htrht41',
     venue: 'Google Meet',
-    fromDate: '2024-10-21 00:00:00',
-    toDate: '2024-10-21 23:59:59',
+    fromDate: currentMonthYear + '-11 00:00:00',
+    toDate: currentMonthYear + '-11 23:59:59',
     color: 'amber',
     isFullDay: true,
   },
@@ -128,8 +138,8 @@ const events = ref([
     participant: 'Sheldon',
     id: '#htrht42',
     venue: 'Google Meet',
-    fromDate: '2024-10-21 00:00:00',
-    toDate: '2024-10-21 23:59:59',
+    fromDate: currentMonthYear + '-07 00:00:00',
+    toDate: currentMonthYear + '-07 23:59:59',
     color: 'amber',
     isFullDay: true,
   },
