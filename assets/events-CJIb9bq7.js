dbb57535dd437bd8b3afbf65acb8341730adb5e6
netchampfaris <netchampfaris@users.noreply.github.com>
2025-04-22 12:54:22 +0000

    deploy: 86bc8e5e25940ee793d5b2b763e18db55aa8c196

---
+++ assets/events-CJIb9bq7.js
@@ -0,0 +1,31 @@
+import { i as defineStore, K as reactive, r as ref, k as watch } from "./vendor-D6kG--Wa.js";
+import { u as useStoryStore } from "./story-hTGm59fv.js";
+import "./GenericMountStory.vue2-CYGj5oLX.js";
+const useEventsStore = defineStore("events", () => {
+  const storyStore = useStoryStore();
+  const events = reactive([]);
+  const unseen = ref(0);
+  function addEvent(event) {
+    events.push(event);
+    unseen.value++;
+  }
+  function reset() {
+    events.length = 0;
+    unseen.value = 0;
+  }
+  watch(() => {
+    var _a;
+    return (_a = storyStore.currentVariant) == null ? void 0 : _a.id;
+  }, () => {
+    reset();
+  });
+  return {
+    addEvent,
+    reset,
+    events,
+    unseen
+  };
+});
+export {
+  useEventsStore
+};
