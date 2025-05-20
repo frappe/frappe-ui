ae1633ae126019bf91975dfe70af738586388c0e
surajshetty3416 <surajshetty3416@users.noreply.github.com>
2025-05-20 10:09:12 +0000

    deploy: 5ed54c3b4af32f010eb507747a554368a1587ba8

---
+++ assets/FileUploader.story-BNCbC51Z.js
@@ -0,0 +1,71 @@
+import { ay as defineComponent, aA as resolveComponent, aB as openBlock, aC as createBlock, aD as withCtx, aE as createVNode, aM as createTextVNode, aN as toDisplayString } from "./vendor-BrUp-vP2.js";
+import { _ as __unplugin_components_1 } from "./FileUploader-ex3Nj9Eg.js";
+import { _ as __unplugin_components_0 } from "./Button-LPo7-Wf2.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./FeatherIcon-DJVUPBWL.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "FileUploader.story",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const validateFileFunction = (fileObject) => {
+    };
+    const onSuccess = (file) => {
+    };
+    const __returned__ = { validateFileFunction, onSuccess, FileUploader: __unplugin_components_1, get Button() {
+      return __unplugin_components_0;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Story = resolveComponent("Story");
+  return openBlock(), createBlock(_component_Story, {
+    layout: { width: 500, type: "grid" },
+    autoPropsDisabled: ""
+  }, {
+    default: withCtx(() => [
+      createVNode($setup["FileUploader"], {
+        fileTypes: ["image/*"],
+        validateFile: $setup.validateFileFunction,
+        onSuccess: $setup.onSuccess
+      }, {
+        default: withCtx(({
+          file,
+          uploading,
+          progress,
+          uploaded,
+          message,
+          error,
+          total,
+          success,
+          openFileSelector
+        }) => [
+          createVNode($setup["Button"], {
+            onClick: openFileSelector,
+            loading: uploading
+          }, {
+            default: withCtx(() => [
+              createTextVNode(
+                toDisplayString(uploading ? `Uploading ${progress}%` : "Upload Image"),
+                1
+                /* TEXT */
+              )
+            ]),
+            _: 2
+            /* DYNAMIC */
+          }, 1032, ["onClick", "loading"])
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 1
+    /* STABLE */
+  });
+}
+_sfc_main.__file = "src/components/FileUploader.story.vue";
+const FileUploader_story = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/FileUploader.story.vue"]]);
+export {
+  FileUploader_story as default
+};
