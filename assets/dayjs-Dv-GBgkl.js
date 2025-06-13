01a924c6de4dc75ef11119d7e660f025d8309b21
surajshetty3416 <surajshetty3416@users.noreply.github.com>
2025-06-13 14:26:30 +0000

    deploy: decf1d435547ae5fda86f363ecbc320e59d0e03c

---
+++ assets/dayjs-Dv-GBgkl.js
@@ -0,0 +1,37 @@
+import { cA as dayjs$1, cB as updateLocale, cC as relativeTime, cD as localizedFormat, cE as isToday, cF as duration, cG as utc, cH as timezone, cI as advancedFormat } from "./vendor-CgURbbe6.js";
+let config = {};
+function getConfig(key) {
+  return config[key] || null;
+}
+dayjs$1.extend(updateLocale);
+dayjs$1.extend(relativeTime);
+dayjs$1.extend(localizedFormat);
+dayjs$1.extend(isToday);
+dayjs$1.extend(duration);
+dayjs$1.extend(utc);
+dayjs$1.extend(timezone);
+dayjs$1.extend(advancedFormat);
+function getBrowserTimezone() {
+  return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+function dayjsLocal(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(localTimezone);
+  return dayjs$1.tz(dateTimeString, systemTimezone).tz(localTimezone);
+}
+function dayjsSystem(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(systemTimezone);
+  return dayjs$1.tz(dateTimeString, localTimezone).tz(systemTimezone);
+}
+let dayjs = dayjs$1;
+export {
+  dayjsLocal as a,
+  dayjsSystem as b,
+  dayjs as d,
+  getConfig as g
+};
