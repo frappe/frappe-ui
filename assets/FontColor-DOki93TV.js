714087e817992b748575384523f84ff86267cfc5
shariquerik <shariquerik@users.noreply.github.com>
2025-06-16 11:58:40 +0000

    deploy: f1a1426f86b90871a9d9eeee1a61e7d87b3a5f59

---
+++ assets/FontColor-DOki93TV.js
@@ -0,0 +1,205 @@
+import { _ as __unplugin_components_1 } from "./Popover-Ds0ThUwV.js";
+import { _ as __unplugin_components_0 } from "./Tooltip-DPB0Ak8C.js";
+import "./ListRow-BenOjeoq.js";
+import "./Autocomplete-DARRvl84.js";
+import "./Avatar-Drx9VoB1.js";
+import "./Badge-XmDjsDcI.js";
+import "./Breadcrumbs-CRZZ8KZX.js";
+import "./Button-BtVPJuYT.js";
+import "./Checkbox-TJ1yE5VN.js";
+import "./DateRangePicker-99HOZmc0.js";
+import "./Dialog-CEcV6Kg8.js";
+import "./Dropdown-DLtjKVWh.js";
+import "./ErrorMessage-soL8rIGr.js";
+import "./FeatherIcon-DQgELGda.js";
+import "./FileUploader-DBmGwi8k.js";
+import "./FormControl-Bi9cvCU9.js";
+import "./Progress-BAl91Xez.js";
+import "./Rating-2vRF5k-L.js";
+import "./Select-7y3J8dip.js";
+import "./Spinner-8Uv_s-vY.js";
+import "./Switch-CU_rZYGO.js";
+import "./TabButtons-DgQ1_1zn.js";
+import "./Tabs-DPonnlHn.js";
+import "./TextInput-DPkUhGXC.js";
+import "./Textarea-CQgC0y5n.js";
+import "./Calendar-DF8TWB8s.js";
+import "./CircularProgressBar-DouLfx8d.js";
+import "./Tree-CVx4y7IN.js";
+import "./NumberChart-CrfEaGuD.js";
+import { aB as openBlock, aC as createBlock, aD as withCtx, aX as renderSlot, aR as normalizeProps, aS as guardReactiveProps, aF as createBaseVNode, aG as createElementBlock, aK as Fragment, aJ as renderList, aQ as normalizeClass } from "./vendor-C34A7lsM.js";
+import "./dayjs-BK3Ou1T8.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./debounce-CRCtzhPg.js";
+const _sfc_main = {
+  name: "FontColor",
+  props: ["editor"],
+  components: { Popover: __unplugin_components_1, Tooltip: __unplugin_components_0 },
+  methods: {
+    setBackgroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().toggleHighlightByName(color.name.toLowerCase()).run();
+      } else {
+        this.editor.chain().focus().unsetHighlight().run();
+      }
+    },
+    setForegroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().setColorByName(color.name.toLowerCase()).run();
+      } else {
+        this.editor.chain().focus().unsetColor().run();
+      }
+    }
+  },
+  computed: {
+    foregroundColors() {
+      return [
+        { name: "Default", class: "text-ink-gray-9" },
+        { name: "Red", class: "text-red-600 dark:text-dark-red-400" },
+        { name: "Orange", class: "text-orange-600 dark:text-dark-orange-400" },
+        { name: "Yellow", class: "text-yellow-600 dark:text-dark-yellow-400" },
+        { name: "Green", class: "text-green-600 dark:text-dark-green-400" },
+        { name: "Teal", class: "text-teal-600 dark:text-dark-teal-400" },
+        { name: "Cyan", class: "text-cyan-600 dark:text-dark-cyan-400" },
+        { name: "Blue", class: "text-blue-600 dark:text-dark-blue-400" },
+        { name: "Purple", class: "text-purple-600 dark:text-dark-purple-400" },
+        { name: "Pink", class: "text-pink-600 dark:text-dark-pink-400" },
+        { name: "Gray", class: "text-gray-600 dark:text-dark-gray-400" }
+      ];
+    },
+    backgroundColors() {
+      return [
+        { name: "Default", class: "border-outline-gray-modals" },
+        {
+          name: "Red",
+          class: "bg-red-100 dark:bg-dark-red-800 border-transparent"
+        },
+        {
+          name: "Orange",
+          class: "bg-orange-100 dark:bg-dark-orange-800 border-transparent"
+        },
+        {
+          name: "Yellow",
+          class: "bg-yellow-100 dark:bg-dark-yellow-800 border-transparent"
+        },
+        {
+          name: "Green",
+          class: "bg-green-100 dark:bg-dark-green-800 border-transparent"
+        },
+        {
+          name: "Teal",
+          class: "bg-teal-100 dark:bg-dark-teal-800 border-transparent"
+        },
+        {
+          name: "Cyan",
+          class: "bg-cyan-100 dark:bg-dark-cyan-800 border-transparent"
+        },
+        {
+          name: "Blue",
+          class: "bg-blue-100 dark:bg-dark-blue-800 border-transparent"
+        },
+        {
+          name: "Purple",
+          class: "bg-purple-100 dark:bg-dark-purple-800 border-transparent"
+        },
+        {
+          name: "Pink",
+          class: "bg-pink-100 dark:bg-dark-pink-800 border-transparent"
+        },
+        {
+          name: "Gray",
+          class: "bg-gray-100 dark:bg-dark-gray-800 border-transparent"
+        }
+      ];
+    }
+  }
+};
+const _hoisted_1 = { class: "p-2" };
+const _hoisted_2 = { class: "mt-1 grid grid-cols-6 gap-1" };
+const _hoisted_3 = ["aria-label", "onClick"];
+const _hoisted_4 = { class: "mt-1 grid grid-cols-6 gap-1" };
+const _hoisted_5 = ["aria-label", "onClick"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Tooltip = __unplugin_components_0;
+  const _component_Popover = __unplugin_components_1;
+  return openBlock(), createBlock(_component_Popover, { transition: "default" }, {
+    target: withCtx(({ togglePopover, isOpen }) => [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: () => togglePopover(), isActive: isOpen })))
+    ]),
+    "body-main": withCtx(() => [
+      createBaseVNode("div", _hoisted_1, [
+        _cache[0] || (_cache[0] = createBaseVNode(
+          "div",
+          { class: "text-sm text-ink-gray-7" },
+          "Text Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_2, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.foregroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: normalizeClass(["flex h-5 w-5 items-center justify-center rounded border text-base", color.class]),
+                    onClick: ($event) => $options.setForegroundColor(color)
+                  }, " A ", 10, _hoisted_3)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ]),
+        _cache[1] || (_cache[1] = createBaseVNode(
+          "div",
+          { class: "mt-2 text-sm text-ink-gray-7" },
+          "Background Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_4, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.backgroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: normalizeClass(["flex h-5 w-5 items-center justify-center rounded border text-base text-ink-gray-9", color.class]),
+                    onClick: ($event) => $options.setBackgroundColor(color)
+                  }, " A ", 10, _hoisted_5)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ])
+      ])
+    ]),
+    _: 3
+    /* FORWARDED */
+  });
+}
+_sfc_main.__file = "src/components/TextEditor/FontColor.vue";
+const FontColor = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/FontColor.vue"]]);
+export {
+  FontColor as default
+};
---
+++ assets/TabButtons-DgQ1_1zn.js
@@ -0,0 +1,116 @@
+import { F as FeatherIcon } from "./FeatherIcon-DQgELGda.js";
+import { cY as he, cZ as Oe, c_ as ke, aA as resolveComponent, aB as openBlock, aC as createBlock, aD as withCtx, aF as createBaseVNode, aG as createElementBlock, aK as Fragment, aJ as renderList, aQ as normalizeClass, aH as createCommentVNode, be as withDirectives, aE as createVNode, aM as createTextVNode, aN as toDisplayString, bf as vShow } from "./vendor-C34A7lsM.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+const _sfc_main = {
+  name: "TabButtons",
+  props: {
+    buttons: {
+      type: Array,
+      required: true
+    },
+    modelValue: {
+      type: [String, Boolean, Number]
+    }
+  },
+  emits: ["update:modelValue"],
+  components: {
+    FeatherIcon,
+    RadioGroup: he,
+    RadioGroupOption: Oe,
+    RadioGroupLabel: ke
+  },
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      }
+    }
+  }
+};
+const _hoisted_1 = { class: "flex space-x-1 rounded bg-surface-gray-2 p-0.5 text-sm" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_FeatherIcon = FeatherIcon;
+  const _component_RadioGroupLabel = resolveComponent("RadioGroupLabel");
+  const _component_RadioGroupOption = resolveComponent("RadioGroupOption");
+  const _component_RadioGroup = resolveComponent("RadioGroup");
+  return openBlock(), createBlock(_component_RadioGroup, {
+    modelValue: $options.value,
+    "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => $options.value = $event)
+  }, {
+    default: withCtx(() => [
+      createBaseVNode("div", _hoisted_1, [
+        (openBlock(true), createElementBlock(
+          Fragment,
+          null,
+          renderList($props.buttons, (button) => {
+            return openBlock(), createBlock(_component_RadioGroupOption, {
+              as: "template",
+              key: button.label,
+              value: button.value ?? button.label
+            }, {
+              default: withCtx(({ active, checked }) => [
+                createBaseVNode(
+                  "button",
+                  {
+                    class: normalizeClass([
+                      active ? "ring-outline-gray-2 focus-visible:ring" : "",
+                      checked ? "bg-surface-white text-ink-gray-9 shadow" : "text-ink-gray-7",
+                      "flex flex-1 justify-center gap-2 whitespace-nowrap rounded-[7px] px-3 py-[5px] leading-none transition-colors focus:outline-none"
+                    ])
+                  },
+                  [
+                    button.icon ? (openBlock(), createBlock(_component_FeatherIcon, {
+                      key: 0,
+                      class: "h-4 w-4",
+                      name: button.icon,
+                      label: button.label,
+                      "aria-label": button.label
+                    }, null, 8, ["name", "label", "aria-label"])) : createCommentVNode("v-if", true),
+                    withDirectives(createVNode(
+                      _component_RadioGroupLabel,
+                      {
+                        as: "span",
+                        class: "flex h-4 items-center"
+                      },
+                      {
+                        default: withCtx(() => [
+                          createTextVNode(
+                            toDisplayString(button.label),
+                            1
+                            /* TEXT */
+                          )
+                        ]),
+                        _: 2
+                        /* DYNAMIC */
+                      },
+                      1536
+                      /* NEED_PATCH, DYNAMIC_SLOTS */
+                    ), [
+                      [vShow, button.label && !button.hideLabel]
+                    ])
+                  ],
+                  2
+                  /* CLASS */
+                )
+              ]),
+              _: 2
+              /* DYNAMIC */
+            }, 1032, ["value"]);
+          }),
+          128
+          /* KEYED_FRAGMENT */
+        ))
+      ])
+    ]),
+    _: 1
+    /* STABLE */
+  }, 8, ["modelValue"]);
+}
+_sfc_main.__file = "src/components/TabButtons/TabButtons.vue";
+const TabButtons = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TabButtons/TabButtons.vue"]]);
+export {
+  TabButtons as T
+};
