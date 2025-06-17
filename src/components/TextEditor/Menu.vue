f1aaea235bbe3abd52af8e13399125a26dc501b8
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-06-17 14:49:10 +0530

    feat: allow searching in submenu

--- src/components/TextEditor/Menu.vue
+++ src/components/TextEditor/Menu.vue
@@ -6,65 +6,8 @@
           class="h-4 w-[2px] border-l"
           v-if="button.type === 'separator'"
         ></div>
-        <div class="shrink-0" v-else-if="button.map">
-          <Popover>
-            <template #target="{ togglePopover }">
-              <button
-                class="rounded px-2 py-1 text-base font-medium text-ink-gray-8 transition-colors"
-                :class="
-                  button.find((o) => o.isActive(editor))
-                    ? 'bg-surface-gray-3'
-                    : 'hover:bg-surface-gray-2'
-                "
-                @click="togglePopover"
-                :set="
-                  (activeBtn =
-                    button.find((b) => b.isActive(editor)) || button[0])
-                "
-              >
-                <component
-                  v-if="activeBtn.icon"
-                  :is="activeBtn.icon"
-                  class="h-4 w-4"
-                />
-                <span v-else>
-                  {{ activeBtn.label }}
-                </span>
-              </button>
-            </template>
-            <template #body="{ close }">
-              <ul class="rounded border bg-surface-white p-1 my-1 shadow-md">
-                <li
-                  class="w-full"
-                  v-for="option in button"
-                  v-show="option.isDisabled ? !option.isDisabled(editor) : true"
-                >
-                  <button
-                    class="w-full flex gap-1 rounded-sm p-1 text-ink-gray-8 transition-colors text-sm"
-                    :class="[
-                      option.isActive(editor)
-                        ? 'bg-surface-gray-3'
-                        : 'hover:bg-surface-gray-2',
-                      option.class,
-                    ]"
-                    @click="
-                      () => {
-                        onButtonClick(option)
-                        close()
-                      }
-                    "
-                  >
-                    <component
-                      v-if="option.icon"
-                      :is="option.icon"
-                      class="size-4"
-                    />
-                    <template v-if="option.label">{{ option.label }}</template>
-                  </button>
-                </li>
-              </ul>
-            </template>
-          </Popover>
+        <div class="shrink-0" v-else-if="button.map || button.group">
+          <SubMenu :editor :button @click="onButtonClick" />
         </div>
         <component v-else :is="button.component || 'div'" v-bind="{ editor }">
           <template v-slot="componentSlotProps">
@@ -98,18 +41,17 @@
   </div>
 </template>
 <script>
-import Popover from '../Popover/Popover.vue'
+import SubMenu from './SubMenu.vue'
 
 export default {
   name: 'TipTapMenu',
   props: ['buttons'],
   inject: ['editor'],
   components: {
-    Popover,
+    SubMenu,
   },
   methods: {
     onButtonClick(button) {
-      console.log(this.buttons)
       button.action(this.editor)
     },
   },
---
+++ src/components/TextEditor/SubMenu.vue
@@ -0,0 +1,80 @@
+<template>
+  <Popover>
+    <template #target="{ togglePopover }">
+      <button
+        class="rounded px-2 py-1 text-base font-medium text-ink-gray-8 transition-colors"
+        :class="
+          actions.find((o) => o.isActive(editor))
+            ? 'bg-surface-gray-3'
+            : 'hover:bg-surface-gray-2'
+        "
+        @click="togglePopover"
+        :set="
+          (activeBtn = actions.find((b) => b.isActive(editor)) || actions[0])
+        "
+      >
+        <component v-if="activeBtn.icon" :is="activeBtn.icon" class="h-4 w-4" />
+        <span v-else>
+          {{ activeBtn.label }}
+        </span>
+      </button>
+    </template>
+    <template #body="{ close }">
+      <div class="rounded border bg-surface-white p-1 my-1 shadow-md">
+        <TextInput v-if="button.search" v-model="search" class="mb-0.5" />
+        <ul :class="button.search && 'max-h-32 overflow-scroll'">
+          <li
+            class="w-full"
+            v-for="option in filteredActions"
+            v-show="option.isDisabled ? !option.isDisabled(editor) : true"
+          >
+            <button
+              class="w-full flex gap-1 rounded-sm p-1 text-ink-gray-8 transition-colors text-sm"
+              :class="[
+                option.isActive(editor)
+                  ? 'bg-surface-gray-3'
+                  : 'hover:bg-surface-gray-2',
+                option.class,
+              ]"
+              @click="
+                () => {
+                  $emit('click', option)
+                  close()
+                }
+              "
+            >
+              <component v-if="option.icon" :is="option.icon" class="size-4" />
+              <template v-if="option.label">{{ option.label }}</template>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </Popover>
+</template>
+<script setup>
+import Popover from '../Popover/Popover.vue'
+import TextInput from '../TextInput/TextInput.vue'
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+  editor: Object,
+  button: [Object, Array],
+})
+
+defineEmits('click')
+
+// Handle both array-based and object-based actions
+const search = ref('')
+const actions = computed(() =>
+  props.button.group ? props.button.actions : props.button
+)
+const filteredActions = computed(() => {
+  console.log(search.value)
+  return props.button.search
+    ? actions.value.filter((k) =>
+        k.label.toLowerCase().includes(search.value.toLowerCase())
+      )
+    : actions.value
+})
+</script>
