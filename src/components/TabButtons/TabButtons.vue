825decd50019f0e5df5a40e042ffffe9619da9dc
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-06-23 10:01:30 +0530

    fix: use frappe ui button

--- src/components/TabButtons/TabButtons.vue
+++ src/components/TabButtons/TabButtons.vue
@@ -1,15 +1,33 @@
 <template>
   <RadioGroup v-model="value">
-    <div class="flex space-x-1 rounded bg-surface-gray-2 p-0.5 text-sm">
+    <div class="flex space-x-1 rounded bg-surface-gray-2 h-7 p-0.5 text-sm">
       <RadioGroupOption
-        as="template"
+        as="div"
         v-for="button in buttons"
         :key="button.label"
         :disabled="button.disabled"
         :value="button.value ?? button.label"
         v-slot="{ active, checked }"
       >
-        <button
+        <Button
+          @click="button.onClick"
+          v-bind="button"
+          class="!h-6"
+          :class="[
+            active ? 'ring-outline-gray-2 focus-visible:ring' : '',
+            checked
+              ? '!bg-surface-white text-ink-gray-9 shadow'
+              : 'text-ink-gray-7',
+          ]"
+        >
+          <RadioGroupLabel
+            as="span"
+            class="flex h-4 items-center"
+            v-show="button.label && !button.hideLabel"
+            >{{ button.label }}</RadioGroupLabel
+          >
+        </Button>
+        <!-- <button
           :class="[
             active ? 'ring-outline-gray-2 focus-visible:ring' : '',
             checked
@@ -17,29 +35,9 @@
               : 'text-ink-gray-7',
             'flex flex-1 justify-center gap-2 whitespace-nowrap rounded-[7px] px-3 py-[5px] leading-none transition-colors focus:outline-none',
           ]"
-          @click="button.onClick"
         >
-          <FeatherIcon
-            class="h-4 w-4"
-            v-if="button.icon && typeof button.icon === 'string'"
-            :name="button.icon"
-            :label="button.label"
-            :aria-label="button.label"
-          />
-          <component
-            v-else-if="button.icon"
-            class="h-4 w-4"
-            :is="button.icon"
-            :label="button.label"
-            :aria-label="button.label"
-          />
-          <RadioGroupLabel
-            as="span"
-            class="flex h-4 items-center"
-            v-show="button.label && !button.hideLabel"
-            >{{ button.label }}</RadioGroupLabel
-          >
-        </button>
+       
+        </button> -->
       </RadioGroupOption>
     </div>
   </RadioGroup>
