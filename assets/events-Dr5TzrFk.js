486d7f42b0d7740237354c71ac1ef4bfa11a9558
shariquerik <shariquerik@users.noreply.github.com>
2025-06-25 11:48:03 +0000

    deploy: 38e36a969f8c353d5053d2c4edbfea22e9e55ad1

---
+++ assets/events-Dr5TzrFk.js
@@ -0,0 +1,31 @@
+import { i as defineStore, K as reactive, r as ref, k as watch } from "./vendor-BG1Ka7Q5.js";
+import { u as useStoryStore } from "./story-ByxAqty3.js";
+import "./GenericMountStory.vue2-NrhKkY8I.js";
+const useEventsStore = defineStore("events", () => {
+  const storyStore = useStoryStore();
+  const events = reactive([]);
+  const unseen = ref(0);
+  function addEvent(event) {
+    events.push(event);
+    unseen.value++;
+  }
+  function reset() {
+    events.length = 0;
+    unseen.value = 0;
+  }
+  watch(() => {
+    var _a;
+    return (_a = storyStore.currentVariant) == null ? void 0 : _a.id;
+  }, () => {
+    reset();
+  });
+  return {
+    addEvent,
+    reset,
+    events,
+    unseen
+  };
+});
+export {
+  useEventsStore
+};
