11d8231799a96cab973b32a3eb1604426e7caeb9
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-06-25 21:42:54 +0530

    feat: add tag input

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,7 @@ export { default as ListGroupRows } from './components/ListView/ListGroupRows.vu
 export { default as ListSelectBanner } from './components/ListView/ListSelectBanner.vue'
 export { default as ListFooter } from './components/ListView/ListFooter.vue'
 export { default as Toast } from './components/Toast/Toast.vue'
+export { default as TagInput } from './components/TagInput.vue'
 export { toast } from './components/Toast/index'
 export * from './components/Tooltip'
 export { default as CommandPalette } from './components/CommandPalette/CommandPalette.vue'
