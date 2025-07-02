f781163f4c950e32f6b59302b53c68b811b912a1
Ritvik Sardana <ritviksardana@Ritviks-MacBook-Air-2.local>
2025-07-02 21:58:33 +0530

    fix: image pasting via clipboard

--- src/components/TextEditor/extensions/markdown-paste-extension.ts
+++ src/components/TextEditor/extensions/markdown-paste-extension.ts
@@ -2,6 +2,8 @@ import { Extension } from '@tiptap/core'
 import { Plugin, PluginKey } from '@tiptap/pm/state'
 import { DOMParser } from '@tiptap/pm/model'
 import { detectMarkdown, markdownToHTML } from '../../../utils/markdown'
+import { EditorView } from 'prosemirror-view'
+import { processMultipleImages } from './image/image-extension'
 
 export interface MarkdownPasteOptions {
   enabled: boolean
@@ -19,12 +21,29 @@ export const MarkdownPasteExtension = Extension.create<MarkdownPasteOptions>({
   },
 
   addProseMirrorPlugins() {
+    const extensionThis = this
     return [
       new Plugin({
         key: new PluginKey('markdownPaste'),
         props: {
-          handlePaste: (view, event, slice) => {
+          handlePaste: (
+            view: EditorView,
+            event: ClipboardEvent,
+            slice: any,
+          ) => {
             if (!this.options.enabled) return false
+
+            // Check for images first
+            const files: File[] | [] = Array.from(
+              event.clipboardData?.files || [],
+            )
+            const images = Array.from(files).filter((file) =>
+              file.type.startsWith('image/'),
+            )
+            if (images.length > 0) {
+              processMultipleImages(images, view, null, extensionThis.options)
+              return true
+            }
 
             const text = event.clipboardData?.getData('text/plain')
             if (!text) return false
