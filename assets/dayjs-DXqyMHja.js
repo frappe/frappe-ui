9d62df4c2de768575c394cbe1e30d7cd276c1f27
nextchamp-saqib <nextchamp-saqib@users.noreply.github.com>
2025-07-09 10:02:46 +0000

    deploy: 1f283caf2ab23c1935af1df93acf2d788bb9b38b

---
+++ assets/dayjs-DXqyMHja.js
@@ -0,0 +1,37 @@
+import { cD as dayjs$1, cE as updateLocale, cF as relativeTime, cG as localizedFormat, cH as isToday, cI as duration, cJ as utc, cK as timezone, cL as advancedFormat } from "./vendor-DRGq_cc5.js";
+let config = {};
+function getConfig(key) {
+  return config[key] ?? null;
+}
+dayjs$1.extend(updateLocale);
+dayjs$1.extend(relativeTime);
+dayjs$1.extend(localizedFormat);
+dayjs$1.extend(isToday);
+dayjs$1.extend(duration);
+dayjs$1.extend(utc);
+dayjs$1.extend(timezone);
+dayjs$1.extend(advancedFormat);
+function getBrowserTimezone() {
+  return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+function dayjsLocal(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(localTimezone);
+  return dayjs$1.tz(dateTimeString, systemTimezone).tz(localTimezone);
+}
+function dayjsSystem(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(systemTimezone);
+  return dayjs$1.tz(dateTimeString, localTimezone).tz(systemTimezone);
+}
+let dayjs = dayjs$1;
+export {
+  dayjsLocal as a,
+  dayjsSystem as b,
+  dayjs as d,
+  getConfig as g
+};
