c1224e2ee7ec0b415b299bebd41c2656e300239e
shariquerik <shariquerik@users.noreply.github.com>
2025-07-15 06:33:10 +0000

    deploy: b295b54aaa3a2a22f455df819d87433dc6b9ff6a

---
+++ assets/FontColor-COyTZB-d.js
@@ -0,0 +1,208 @@
+import { _ as __unplugin_components_1 } from "./Popover-BwspgZkz.js";
+import { _ as __unplugin_components_0 } from "./Tooltip-BPk2Zsag.js";
+import "./ListRow-D-OWoXHY.js";
+import "./Autocomplete-IDfBIoZ5.js";
+import "./Avatar-CHgSav_C.js";
+import "./Badge-BBDGRmcd.js";
+import "./Breadcrumbs-BdNg1UUu.js";
+import "./Button-BU6pU2Fc.js";
+import "./Combobox-vBcY2KL_.js";
+import "./Checkbox-BlxSz9B-.js";
+import "./DateRangePicker-Gz2uo3ps.js";
+import "./Dialog-H3DzfJn_.js";
+import "./Dropdown-D7q8J7cF.js";
+import "./ErrorMessage-q72oz_Fj.js";
+import "./FeatherIcon-Blud0Soc.js";
+import "./FileUploader-CTZJDqYJ.js";
+import "./FormControl-B9YS41mk.js";
+import "./Progress-DjNPgqGK.js";
+import "./Rating-CWmSHbVe.js";
+import "./Select-CM2c-URU.js";
+import "./Spinner-zt0SAuqv.js";
+import "./Switch-DGtg_1Zg.js";
+import "./TabButtons-DMHKFo-6.js";
+import "./Tabs-DOxFcc8B.js";
+import "./TextInput-Bt7Vtr6f.js";
+import "./Textarea-WeLfZxkY.js";
+import "./Calendar-BLC0QncJ.js";
+import "./CircularProgressBar-CT8pmo2-.js";
+import "./Tree-4458JPbv.js";
+import "./Sidebar-DJCE_QD7.js";
+import "./NumberChart-DEL23PzX.js";
+import { aB as openBlock, aC as createBlock, aD as withCtx, aZ as renderSlot, aR as normalizeProps, aS as guardReactiveProps, aF as createBaseVNode, aG as createElementBlock, aK as Fragment, aJ as renderList, aQ as normalizeClass } from "./vendor-Y_wWrJTg.js";
+import "./dayjs-Hp0LBMvR.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./debounce-CRCtzhPg.js";
+import "./chevron-down-jU8IVr1M.js";
+const _sfc_main = {
+  name: "FontColor",
+  props: ["editor"],
+  components: { Popover: __unplugin_components_1, Tooltip: __unplugin_components_0 },
+  methods: {
+    setBackgroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().toggleHighlightByName(color.name.toLowerCase()).run();
+      } else {
+        this.editor.chain().focus().unsetHighlight().run();
+      }
+    },
+    setForegroundColor(color) {
+      if (color.name != "Default") {
+        this.editor.chain().focus().setColorByName(color.name.toLowerCase()).run();
+      } else {
+        this.editor.chain().focus().unsetColor().run();
+      }
+    }
+  },
+  computed: {
+    foregroundColors() {
+      return [
+        { name: "Default", class: "text-ink-gray-9" },
+        { name: "Red", class: "text-red-600 dark:text-dark-red-400" },
+        { name: "Orange", class: "text-orange-600 dark:text-dark-orange-400" },
+        { name: "Yellow", class: "text-yellow-600 dark:text-dark-yellow-400" },
+        { name: "Green", class: "text-green-600 dark:text-dark-green-400" },
+        { name: "Teal", class: "text-teal-600 dark:text-dark-teal-400" },
+        { name: "Cyan", class: "text-cyan-600 dark:text-dark-cyan-400" },
+        { name: "Blue", class: "text-blue-600 dark:text-dark-blue-400" },
+        { name: "Purple", class: "text-purple-600 dark:text-dark-purple-400" },
+        { name: "Pink", class: "text-pink-600 dark:text-dark-pink-400" },
+        { name: "Gray", class: "text-gray-600 dark:text-dark-gray-400" }
+      ];
+    },
+    backgroundColors() {
+      return [
+        { name: "Default", class: "border-outline-gray-modals" },
+        {
+          name: "Red",
+          class: "bg-red-100 dark:bg-dark-red-800 border-transparent"
+        },
+        {
+          name: "Orange",
+          class: "bg-orange-100 dark:bg-dark-orange-800 border-transparent"
+        },
+        {
+          name: "Yellow",
+          class: "bg-yellow-100 dark:bg-dark-yellow-800 border-transparent"
+        },
+        {
+          name: "Green",
+          class: "bg-green-100 dark:bg-dark-green-800 border-transparent"
+        },
+        {
+          name: "Teal",
+          class: "bg-teal-100 dark:bg-dark-teal-800 border-transparent"
+        },
+        {
+          name: "Cyan",
+          class: "bg-cyan-100 dark:bg-dark-cyan-800 border-transparent"
+        },
+        {
+          name: "Blue",
+          class: "bg-blue-100 dark:bg-dark-blue-800 border-transparent"
+        },
+        {
+          name: "Purple",
+          class: "bg-purple-100 dark:bg-dark-purple-800 border-transparent"
+        },
+        {
+          name: "Pink",
+          class: "bg-pink-100 dark:bg-dark-pink-800 border-transparent"
+        },
+        {
+          name: "Gray",
+          class: "bg-gray-100 dark:bg-dark-gray-800 border-transparent"
+        }
+      ];
+    }
+  }
+};
+const _hoisted_1 = { class: "p-2" };
+const _hoisted_2 = { class: "mt-1 grid grid-cols-6 gap-1" };
+const _hoisted_3 = ["aria-label", "onClick"];
+const _hoisted_4 = { class: "mt-1 grid grid-cols-6 gap-1" };
+const _hoisted_5 = ["aria-label", "onClick"];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Tooltip = __unplugin_components_0;
+  const _component_Popover = __unplugin_components_1;
+  return openBlock(), createBlock(_component_Popover, { transition: "default" }, {
+    target: withCtx(({ togglePopover, isOpen }) => [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: () => togglePopover(), isActive: isOpen })))
+    ]),
+    "body-main": withCtx(() => [
+      createBaseVNode("div", _hoisted_1, [
+        _cache[0] || (_cache[0] = createBaseVNode(
+          "div",
+          { class: "text-sm text-ink-gray-7" },
+          "Text Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_2, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.foregroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: normalizeClass(["flex h-5 w-5 items-center justify-center rounded border text-base", color.class]),
+                    onClick: ($event) => $options.setForegroundColor(color)
+                  }, " A ", 10, _hoisted_3)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ]),
+        _cache[1] || (_cache[1] = createBaseVNode(
+          "div",
+          { class: "mt-2 text-sm text-ink-gray-7" },
+          "Background Color",
+          -1
+          /* HOISTED */
+        )),
+        createBaseVNode("div", _hoisted_4, [
+          (openBlock(true), createElementBlock(
+            Fragment,
+            null,
+            renderList($options.backgroundColors, (color) => {
+              return openBlock(), createBlock(_component_Tooltip, {
+                class: "flex",
+                key: color.name,
+                text: color.name
+              }, {
+                default: withCtx(() => [
+                  createBaseVNode("button", {
+                    "aria-label": color.name,
+                    class: normalizeClass(["flex h-5 w-5 items-center justify-center rounded border text-base text-ink-gray-9", color.class]),
+                    onClick: ($event) => $options.setBackgroundColor(color)
+                  }, " A ", 10, _hoisted_5)
+                ]),
+                _: 2
+                /* DYNAMIC */
+              }, 1032, ["text"]);
+            }),
+            128
+            /* KEYED_FRAGMENT */
+          ))
+        ])
+      ])
+    ]),
+    _: 3
+    /* FORWARDED */
+  });
+}
+_sfc_main.__file = "src/components/TextEditor/FontColor.vue";
+const FontColor = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/FontColor.vue"]]);
+export {
+  FontColor as default
+};
---
+++ assets/InsertImage-B3DAYEwy.js
@@ -0,0 +1,112 @@
+import { ay as defineComponent, be as useTemplateRef, az as ref, aB as openBlock, aG as createElementBlock, aZ as renderSlot, aR as normalizeProps, aS as guardReactiveProps, aF as createBaseVNode, aC as createBlock, aH as createCommentVNode, aK as Fragment } from "./vendor-Y_wWrJTg.js";
+import { I as ImageGroupUploadDialog } from "./ListRow-D-OWoXHY.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./Checkbox-BlxSz9B-.js";
+import "./Autocomplete-IDfBIoZ5.js";
+import "./Popover-BwspgZkz.js";
+import "./Button-BU6pU2Fc.js";
+import "./FeatherIcon-Blud0Soc.js";
+import "./Avatar-CHgSav_C.js";
+import "./Badge-BBDGRmcd.js";
+import "./Breadcrumbs-BdNg1UUu.js";
+import "./Dropdown-D7q8J7cF.js";
+import "./Combobox-vBcY2KL_.js";
+import "./chevron-down-jU8IVr1M.js";
+import "./DateRangePicker-Gz2uo3ps.js";
+import "./TextInput-Bt7Vtr6f.js";
+import "./debounce-CRCtzhPg.js";
+import "./dayjs-Hp0LBMvR.js";
+import "./Dialog-H3DzfJn_.js";
+import "./ErrorMessage-q72oz_Fj.js";
+import "./FileUploader-CTZJDqYJ.js";
+import "./FormControl-B9YS41mk.js";
+import "./Select-CM2c-URU.js";
+import "./Textarea-WeLfZxkY.js";
+import "./Progress-DjNPgqGK.js";
+import "./Rating-CWmSHbVe.js";
+import "./Spinner-zt0SAuqv.js";
+import "./Switch-DGtg_1Zg.js";
+import "./TabButtons-DMHKFo-6.js";
+import "./Tabs-DOxFcc8B.js";
+import "./Tooltip-BPk2Zsag.js";
+import "./Calendar-BLC0QncJ.js";
+import "./CircularProgressBar-CT8pmo2-.js";
+import "./Tree-4458JPbv.js";
+import "./Sidebar-DJCE_QD7.js";
+import "./NumberChart-DEL23PzX.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "InsertImage",
+  props: {
+    editor: { type: Object, required: true }
+  },
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const props = __props;
+    const fileInput = useTemplateRef("fileInput");
+    const showModal = ref(false);
+    const selectedFiles = ref([]);
+    function openFileSelector() {
+      var _a;
+      (_a = fileInput.value) == null ? void 0 : _a.click();
+    }
+    function onImageSelect(e) {
+      const target = e.target;
+      const files = target.files;
+      if (files && files.length > 0) {
+        if (files.length === 1) {
+          props.editor.chain().focus().uploadImage(files[0]).run();
+        } else {
+          selectedFiles.value = Array.from(files);
+          showModal.value = true;
+        }
+      }
+    }
+    function handleCancel() {
+      showModal.value = false;
+      selectedFiles.value = [];
+    }
+    const __returned__ = { props, fileInput, showModal, selectedFiles, openFileSelector, onImageSelect, handleCancel, ImageGroupUploadDialog };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock(
+    Fragment,
+    null,
+    [
+      renderSlot(_ctx.$slots, "default", normalizeProps(guardReactiveProps({ onClick: $setup.openFileSelector }))),
+      createBaseVNode(
+        "input",
+        {
+          ref: "fileInput",
+          type: "file",
+          class: "hidden",
+          onChange: $setup.onImageSelect,
+          accept: "image/*",
+          multiple: ""
+        },
+        null,
+        544
+        /* NEED_HYDRATION, NEED_PATCH */
+      ),
+      $setup.showModal ? (openBlock(), createBlock($setup["ImageGroupUploadDialog"], {
+        key: 0,
+        mode: "new",
+        modelValue: $setup.showModal,
+        "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => $setup.showModal = $event),
+        files: $setup.selectedFiles,
+        "onUpdate:files": _cache[1] || (_cache[1] = ($event) => $setup.selectedFiles = $event),
+        editor: $props.editor,
+        onClose: $setup.handleCancel
+      }, null, 8, ["modelValue", "files", "editor"])) : createCommentVNode("v-if", true)
+    ],
+    64
+    /* STABLE_FRAGMENT */
+  );
+}
+_sfc_main.__file = "src/components/TextEditor/InsertImage.vue";
+const InsertImage = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/TextEditor/InsertImage.vue"]]);
+export {
+  InsertImage as default
+};
