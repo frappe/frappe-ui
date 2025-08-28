14594c8f21e31cf803339e129c4e52e844e08f04
Shariq Ansari <sharique.rik@gmail.com>
2025-08-28 14:11:33 +0530

    feat: add DateMonthYearPicker component and integrate with Calendar

--- src/components/Calendar/index.ts
+++ src/components/Calendar/index.ts
@@ -1,3 +1,4 @@
 export { default as Calendar } from './Calendar.vue'
+export { default as DateMonthYearPicker } from './DateMonthYearPicker.vue'
 export { colorMap as CalendarColorMap } from './calendarUtils.js'
 export { activeEvent as CalendarActiveEvent } from './composables/useCalendarData.js'
