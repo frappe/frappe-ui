27a4d6681968c9112a04cf3b29ee2ec02d50b3ac
shariquerik <shariquerik@users.noreply.github.com>
2025-08-29 09:34:40 +0000

    deploy: 00e47d7836f03e95c227325d67c99706a8318a15

---
+++ assets/events-9-lcNvAa.js
@@ -0,0 +1,31 @@
+import { i as defineStore, K as reactive, r as ref, k as watch } from "./vendor-Dk0c65vB.js";
+import { u as useStoryStore } from "./story-CW0c41D0.js";
+import "./GenericMountStory.vue2-BS0oi_XP.js";
+const useEventsStore = defineStore("events", () => {
+  const storyStore = useStoryStore();
+  const events = reactive([]);
+  const unseen = ref(0);
+  function addEvent(event) {
+    events.push(event);
+    unseen.value++;
+  }
+  function reset() {
+    events.length = 0;
+    unseen.value = 0;
+  }
+  watch(() => {
+    var _a;
+    return (_a = storyStore.currentVariant) == null ? void 0 : _a.id;
+  }, () => {
+    reset();
+  });
+  return {
+    addEvent,
+    reset,
+    events,
+    unseen
+  };
+});
+export {
+  useEventsStore
+};
