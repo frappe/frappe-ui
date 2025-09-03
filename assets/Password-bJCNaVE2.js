7527e06ad8b2846b207fd3aef575cf6fd01c9e19
netchampfaris <netchampfaris@users.noreply.github.com>
2025-09-03 21:20:58 +0000

    deploy: 32a1904f51e871a7b7cedb28ed97bc3aaee8a9ca

---
+++ assets/Password-bJCNaVE2.js
@@ -0,0 +1,323 @@
+import { aP as markRaw, aB as openBlock, aG as createElementBlock, aF as createBaseVNode, ay as defineComponent, aC as createBlock, aH as createCommentVNode, aE as createVNode, aZ as renderSlot, aR as normalizeClass, az as ref, aW as computed, cJ as createSlots, aD as withCtx, aM as createTextVNode, aN as toDisplayString, b3 as withDirectives, bx as resolveDynamicComponent, by as vShow, aL as mergeProps, bm as withKeys, a_ as withModifiers } from "./vendor-DsK-IN2t.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import { F as FormControl } from "./FormControl-BvTwNlRo.js";
+import { _ as __unplugin_components_0 } from "./Button-CcSMnls4.js";
+const _hoisted_1$6 = {
+  class: "lucide lucide-eye",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render$4(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$6, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "circle",
+      {
+        cx: "12",
+        cy: "12",
+        r: "3"
+      },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideEye = markRaw({ name: "lucide-eye", render: render$4 });
+const _hoisted_1$5 = {
+  class: "lucide lucide-eye-off",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render$3(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$5, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "M14.084 14.158a3 3 0 0 1-4.242-4.242" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "m2 2 20 20" },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideEyeOff = markRaw({ name: "lucide-eye-off", render: render$3 });
+const _hoisted_1$4 = {
+  class: "lucide lucide-command",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render$2(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$4, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideCommand = markRaw({ name: "lucide-command", render: render$2 });
+const _hoisted_1$3 = {
+  class: "lucide lucide-arrow-big-up",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render$1(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$3, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M9 13a1 1 0 0 0-1-1H5.061a1 1 0 0 1-.75-1.811l6.836-6.835a1.207 1.207 0 0 1 1.707 0l6.835 6.835a1 1 0 0 1-.75 1.811H16a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1z" },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideShift = markRaw({ name: "lucide-arrow-big-up", render: render$1 });
+const _hoisted_1$2 = {
+  class: "lucide lucide-option",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$2, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M3 3h6l6 18h6" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "M14 3h7" },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideAlt = markRaw({ name: "lucide-option", render });
+const _sfc_main$1 = /* @__PURE__ */ defineComponent({
+  __name: "KeyboardShortcut",
+  props: {
+    meta: Boolean,
+    ctrl: Boolean,
+    shift: Boolean,
+    alt: Boolean,
+    shortcut: String,
+    bg: Boolean
+  },
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const isMac = navigator.userAgent.includes("Mac");
+    const __returned__ = { isMac, get LucideCommand() {
+      return LucideCommand;
+    }, get LucideShift() {
+      return LucideShift;
+    }, get LucideAlt() {
+      return LucideAlt;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1$1 = { key: 0 };
+const _hoisted_2$1 = { key: 1 };
+const _hoisted_3 = { key: 1 };
+const _hoisted_4 = { key: 2 };
+function _sfc_render$1(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock(
+    "div",
+    {
+      class: normalizeClass(["inline-flex items-center gap-0.5 text-sm", {
+        "bg-surface-gray-2 rounded-sm text-ink-gray-5 py-0.5 px-1": $props.bg,
+        "text-ink-gray-4": !$props.bg
+      }])
+    },
+    [
+      $props.ctrl || $props.meta ? (openBlock(), createElementBlock("span", _hoisted_1$1, [
+        $setup.isMac ? (openBlock(), createBlock($setup["LucideCommand"], {
+          key: 0,
+          class: "w-3 h-3"
+        })) : (openBlock(), createElementBlock("span", _hoisted_2$1, "Ctrl"))
+      ])) : createCommentVNode("v-if", true),
+      $props.shift ? (openBlock(), createElementBlock("span", _hoisted_3, [
+        createVNode($setup["LucideShift"], { class: "w-3 h-3" })
+      ])) : createCommentVNode("v-if", true),
+      $props.alt ? (openBlock(), createElementBlock("span", _hoisted_4, [
+        createVNode($setup["LucideAlt"], { class: "w-3 h-3" })
+      ])) : createCommentVNode("v-if", true),
+      renderSlot(_ctx.$slots, "default")
+    ],
+    2
+    /* CLASS */
+  );
+}
+_sfc_main$1.__file = "src/components/KeyboardShortcut.vue";
+const KeyboardShortcut = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render$1], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/KeyboardShortcut.vue"]]);
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "Password",
+  props: {
+    modelValue: { type: [String, null], required: false },
+    value: { type: [String, null], required: false }
+  },
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const props = __props;
+    const show = ref(false);
+    const showEye = computed(() => {
+      let v = props.modelValue || props.value;
+      return !(v == null ? void 0 : v.includes("*"));
+    });
+    const __returned__ = { props, show, showEye, get LucideEye() {
+      return LucideEye;
+    }, get LucideEyeOff() {
+      return LucideEyeOff;
+    }, KeyboardShortcut, FormControl, Tooltip: __unplugin_components_0 };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1 = { class: "rounded bg-surface-gray-7 py-1.5 px-2 text-xs text-ink-white shadow-xl" };
+const _hoisted_2 = { class: "flex items-center gap-1" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createBlock($setup["FormControl"], mergeProps({
+    type: $setup.show ? "text" : "password",
+    value: $props.modelValue || $props.value
+  }, _ctx.$attrs, {
+    onKeydown: [
+      _cache[1] || (_cache[1] = withKeys(withModifiers(($event) => $setup.show = !$setup.show, ["meta", "prevent"]), ["i"])),
+      _cache[2] || (_cache[2] = withKeys(withModifiers(($event) => $setup.show = !$setup.show, ["ctrl", "prevent"]), ["i"]))
+    ]
+  }), createSlots({
+    suffix: withCtx(() => [
+      createVNode($setup["Tooltip"], null, {
+        body: withCtx(() => [
+          createBaseVNode("div", _hoisted_1, [
+            createBaseVNode("span", _hoisted_2, [
+              createTextVNode(
+                toDisplayString($setup.show ? "Hide Password" : "Show Password") + " ",
+                1
+                /* TEXT */
+              ),
+              createVNode($setup["KeyboardShortcut"], {
+                bg: "",
+                ctrl: "",
+                class: "!bg-surface-gray-5 !text-ink-gray-2 px-1"
+              }, {
+                default: withCtx(() => _cache[3] || (_cache[3] = [
+                  createBaseVNode(
+                    "span",
+                    { class: "font-mono leading-none tracking-widest" },
+                    "+I",
+                    -1
+                    /* HOISTED */
+                  )
+                ])),
+                _: 1
+                /* STABLE */
+              })
+            ])
+          ])
+        ]),
+        default: withCtx(() => [
+          createBaseVNode("div", null, [
+            withDirectives((openBlock(), createBlock(
+              resolveDynamicComponent($setup.show ? $setup.LucideEyeOff : $setup.LucideEye),
+              {
+                class: "h-3 cursor-pointer mr-1",
+                onClick: _cache[0] || (_cache[0] = ($event) => $setup.show = !$setup.show)
+              },
+              null,
+              512
+              /* NEED_PATCH */
+            )), [
+              [vShow, $setup.showEye]
+            ])
+          ])
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 2
+    /* DYNAMIC */
+  }, [
+    _ctx.$slots.prefix ? {
+      name: "prefix",
+      fn: withCtx(() => [
+        renderSlot(_ctx.$slots, "prefix")
+      ]),
+      key: "0"
+    } : void 0
+  ]), 1040, ["type", "value"]);
+}
+_sfc_main.__file = "src/components/Password/Password.vue";
+const Password = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/Password/Password.vue"]]);
+export {
+  Password as P
+};
