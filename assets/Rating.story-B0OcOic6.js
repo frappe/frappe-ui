d32ab53c8ba44491b1884cfde29c4a3eec767fe3
krantheman <krantheman@users.noreply.github.com>
2025-10-10 11:26:25 +0000

    deploy: 228544ebb41f424ef9ce54788a7b7f2bfc2a116f

---
+++ assets/Rating.story-B0OcOic6.js
@@ -0,0 +1,48 @@
+import { ay as defineComponent, aI as reactive, aA as resolveComponent, aB as openBlock, aC as createBlock, aD as withCtx, aE as createVNode, aF as createBaseVNode, aS as normalizeProps, aT as guardReactiveProps } from "./vendor-ZKdfit2u.js";
+import { R as Rating } from "./Rating-CpIhp_ld.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./FeatherIcon-_ZA44pHK.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "Rating.story",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const state = reactive({
+      size: "md",
+      label: "Rating"
+    });
+    const __returned__ = { state, Rating };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1 = { class: "p-2" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Variant = resolveComponent("Variant");
+  const _component_Story = resolveComponent("Story");
+  return openBlock(), createBlock(_component_Story, { layout: { type: "grid", width: 300 } }, {
+    default: withCtx(() => [
+      createVNode(_component_Variant, { title: "default" }, {
+        default: withCtx(() => [
+          createBaseVNode("div", _hoisted_1, [
+            createVNode(
+              $setup["Rating"],
+              normalizeProps(guardReactiveProps($setup.state)),
+              null,
+              16
+              /* FULL_PROPS */
+            )
+          ])
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 1
+    /* STABLE */
+  });
+}
+_sfc_main.__file = "src/components/Rating/Rating.story.vue";
+const Rating_story = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/Rating/Rating.story.vue"]]);
+export {
+  Rating_story as default
+};
