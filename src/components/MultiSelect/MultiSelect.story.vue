00d0acee985eae6c1e8c33331fef6a25aebf01d4
siduck <siduck@tutanota.com>
2025-11-11 10:45:13 +0530

    fix: remove useless buttons & include story

---
+++ src/components/MultiSelect/MultiSelect.story.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import MultiSelect from "./MultiSelect.vue";
+
+const state = ref();
+
+const options = [
+  { value: "red-apple", label: "Red Apple" },
+  { value: "blueberry-burst", label: "Blueberry Burst" },
+  { value: "orange-grove", label: "Orange Grove" },
+  { value: "banana-split", label: "Banana Split" },
+  { value: "grapes-cluster", label: "Grapes Cluster" },
+  { value: "kiwi-slice", label: "Kiwi Slice" },
+  { value: "mango-fusion", label: "Mango Fusion" },
+];
+</script>
+
+<template>
+  <Story :layout='{ width: 500, type: "grid" }'>
+    <Variant title="Default">
+      <MultiSelect
+        :options="options"
+        v-model="state"
+        placeholder="Select person"
+      />
+    </Variant>
+  </Story>
+</template>
--- src/components/MultiSelect/MultiSelect.vue
+++ src/components/MultiSelect/MultiSelect.vue
@@ -15,7 +15,6 @@ import {
 import LucideX from "~icons/lucide/x";
 import Popover from "../Popover/Popover.vue";
 import LucideCheck from "~icons/lucide/check";
-import LucideDoubleCheck from "~icons/lucide/check-check";
 import LoadingIndicator from "../LoadingIndicator.vue";
 import LucideChevronDown from "~icons/lucide/chevron-down";
 
@@ -23,20 +22,26 @@ const props = withDefaults(defineProps<MultiSelectProps>(), {
   placeholder: "Select option",
 });
 
-const model = defineModel<MultiSelectOption[]>({ default: [] });
+const model = defineModel<String[]>({ default: [] });
+
+const getValues = (arr: String[]) =>
+  arr.map((x) => props.options.find((y) => y.value === x)?.label);
+
+const optionToStr = (options: MultiSelectOption[]) =>
+  options.map((x) => x.label);
 
 const selectedOptions = computed(() => {
-  const list = model.value?.map((x) => x.label);
-  return list.length > 0 ? list.join(", ") : props.placeholder;
+  const values = getValues(model.value).join(", ");
+  return model.value.length > 0 ? values : props.placeholder;
 });
 
 const clearAll = () => model.value = [];
-const selectAll = () => model.value = props.options;
+const selectAll = () => model.value = optionToStr(props.options);
 </script>
 
 <template>
   <Popover
-    popover-class="mt-2 shadow-xl rounded-lg border"
+    popover-class="mt-2 shadow-xl rounded-lg border bg-surface-modal"
     :matchTargetWidth="true"
   >
     <template #target="{ togglePopover }">
@@ -95,8 +100,8 @@ const selectAll = () => model.value = props.options;
             <ComboboxItem
               v-for="item in props.options"
               :key="item.value"
-              :value="item"
-							:disabled="item.disabled"
+              :value="item.value"
+              :disabled="item.disabled"
               class="
                 text-base leading-none text-ink-gray-7 rounded flex items-center h-7 p-1.5
                 relative select-none data-[disabled]:opacity-50 data-[disabled]:pointer-events-none
@@ -117,18 +122,8 @@ const selectAll = () => model.value = props.options;
 
           <!-- footer btns -->
           <div class="flex justify-between my-2">
-            <Button variant="ghost" @click="clearAll">
-              <template #prefix>
-                <LucideX class="size-4" />
-              </template>
-              Clear All
-            </Button>
-
-            <Button @click="selectAll">
-              <template #prefix>
-                <LucideDoubleCheck class="size-4 text-ink-gray-5" />
-              </template> Select All
-            </Button>
+            <Button variant="ghost" @click="clearAll"> Clear All</Button>
+            <Button @click="selectAll"> Select All</Button>
           </div>
         </ComboboxContent>
       </ComboboxRoot>
