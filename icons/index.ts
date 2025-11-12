63e079d6c781414b065040993e588dbbd0d6a9e4
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-11-12 21:55:08 +0530

    fix: handle more icons

--- icons/index.ts
+++ icons/index.ts
@@ -1,5 +1,6 @@
 export { default as CircleCheckIcon } from './CircleCheckIcon.vue'
 export { default as DownSolidIcon } from './DownSolidIcon.vue'
+export { default as GreenCheckIcon } from './GreenCheckIcon.vue'
 
 // Frappe Icons
 export { default as HelpIcon } from './HelpIcon.vue'
