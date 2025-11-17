08ca31d8642119895a515c06efe5bf7fdb36358a
siduck <siduck@tutanota.com>
2025-11-17 12:47:12 +0530

    add tab-item slot in tabs component

--- src/components/Tabss/Tabs.vue
+++ src/components/Tabss/Tabs.vue
@@ -14,23 +14,31 @@ const props = defineProps<TabProps>()
 
 <template>
   <TabsRoot
-    class="flex flex-col w-full sm:w-[300px] shadow-sm rounded-lg border"
+    class="rounded-lg border border-outline-gray-1"
     :default-value="props.tabs[0].label"
   >
-    <TabsList class="relative shrink-0 flex gap-3.5 px-3.5 border-b">
+    <TabsList
+      class="relative shrink-0 flex gap-3.5 px-3.5 border-b border-outline-gray-1"
+    >
       <TabsIndicator
         class="absolute left-0 h-[2px] bottom-0 w-[--reka-tabs-indicator-size] translate-x-[--reka-tabs-indicator-position] translate-y-[1px] rounded-full transition-[width,transform] duration-300"
       >
-        <div class="bg-grass8 w-full h-full bg-red-400" />
+        <div class="bg-grass8 w-full h-full bg-surface-gray-7" />
       </TabsIndicator>
 
       <TabsTrigger
+        as="template"
         v-for="(item, i) in props.tabs"
-        class="flex items-center gap-1.5 text-base text-ink-gray-5 duration-300 ease-in-out hover:text-ink-gray-9 py-3"
-        :class="['data-[state-active]:text-ink-gray-9']"
         :value="item.label"
       >
-        {{ item.label }}
+        <slot name="tab-item" v-bind="{ item }">
+          <button
+            class="flex items-center gap-1.5 text-base text-ink-gray-5 duration-300 ease-in-out hover:text-ink-gray-9 py-3"
+            :class="['data-[state=active]:text-ink-gray-9']"
+          >
+            {{ item.label }}
+          </button>
+        </slot>
       </TabsTrigger>
     </TabsList>
 
