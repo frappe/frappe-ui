646c4081f6505fe7f1459be3e235f86b4c92e6ec
siduck <siduck@tutanota.com>
2025-11-25 12:57:00 +0530

    include prefix feature example in story

--- src/components/Select/Select.story.vue
+++ src/components/Select/Select.story.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Select from './Select.vue'
+import LucideUser from '~icons/lucide/user'
 
 const value = ref('')
 const options = [
@@ -14,8 +15,20 @@ const options = [
 </script>
 <template>
   <Story :layout="{ width: 500, type: 'grid' }">
-    <div class="p-2">
-      <Select :options="options" v-model="value" />
-    </div>
+    <Variant title="Default">
+      <div class="p-2">
+        <Select :options="options" v-model="value" />
+      </div>
+    </Variant>
+
+    <Variant title="With prefix">
+      <div class="p-2">
+        <Select :options="options" v-model="value">
+          <template #prefix>
+            <LucideUser class="size-4 text-ink-gray-9" />
+          </template>
+        </Select>
+      </div>
+    </Variant>
   </Story>
 </template>
