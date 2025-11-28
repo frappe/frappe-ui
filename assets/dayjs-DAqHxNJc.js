64e46ee6b299bff38592bf8377e4cff58a9c2213
siduck <siduck@users.noreply.github.com>
2025-11-28 11:03:55 +0000

    deploy: 37bc597d7f108b240a8b2dd10774b184414f2a85

---
+++ assets/dayjs-DAqHxNJc.js
@@ -0,0 +1,37 @@
+import { by as dayjs$1, bz as updateLocale, bA as relativeTime, bB as localizedFormat, bC as isToday, bD as duration, bE as utc, bF as timezone, bG as advancedFormat, bH as customParseFormat } from "./vendor-Z3Z6twb9.js";
+let config = {};
+function getConfig(key) {
+  return config[key] ?? null;
+}
+dayjs$1.extend(updateLocale);
+dayjs$1.extend(relativeTime);
+dayjs$1.extend(localizedFormat);
+dayjs$1.extend(isToday);
+dayjs$1.extend(duration);
+dayjs$1.extend(utc);
+dayjs$1.extend(timezone);
+dayjs$1.extend(advancedFormat);
+dayjs$1.extend(customParseFormat);
+function getBrowserTimezone() {
+  return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+function dayjsLocal(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(localTimezone);
+  return dayjs$1.tz(dateTimeString, systemTimezone).tz(localTimezone);
+}
+function dayjsSystem(dateTimeString) {
+  let systemTimezone = getConfig("systemTimezone");
+  let localTimezone = getConfig("localTimezone") || getBrowserTimezone();
+  if (!systemTimezone) return dayjs$1(dateTimeString);
+  if (!dateTimeString) return dayjs$1().tz(systemTimezone);
+  return dayjs$1.tz(dateTimeString, localTimezone).tz(systemTimezone);
+}
+let dayjs = dayjs$1;
+export {
+  dayjsLocal as a,
+  dayjsSystem as b,
+  dayjs as d
+};
---
+++ assets/index-CIxYzgv8.js
@@ -0,0 +1,8 @@
+import { _ as __unplugin_components_1 } from "./Dialog-BEYVo961.js";
+import { co as DialogTitle_default, cp as DialogDescription_default } from "./vendor-Z3Z6twb9.js";
+const Dialog = __unplugin_components_1;
+Dialog.Title = DialogTitle_default;
+Dialog.Description = DialogDescription_default;
+export {
+  Dialog as D
+};
