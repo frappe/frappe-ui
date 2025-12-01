4329b9a351cc688137e81c77820f4c1a24fba729
siduck <siduck@users.noreply.github.com>
2025-12-01 11:41:45 +0000

    deploy: 7a60d4461af9ef09eec657c70c5e3060b53e0484

---
+++ assets/index-CSiVBDuz.js
@@ -0,0 +1,8 @@
+import { _ as __unplugin_components_1 } from "./Dialog-BNZ2iw_S.js";
+import { co as DialogTitle_default, cp as DialogDescription_default } from "./vendor-DHfc0FDQ.js";
+const Dialog = __unplugin_components_1;
+Dialog.Title = DialogTitle_default;
+Dialog.Description = DialogDescription_default;
+export {
+  Dialog as D
+};
