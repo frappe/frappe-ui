9acb70d72afa4adb401c312f1d71cdcd736ead1c
siduck <siduck@users.noreply.github.com>
2025-12-02 11:04:11 +0000

    deploy: 1d6aea6055def2adc5cf41d13576d9a7c28497d1

---
+++ assets/index-CFwcrC6d.js
@@ -0,0 +1,8 @@
+import { _ as __unplugin_components_1 } from "./Dialog-gK-ggTBZ.js";
+import { co as DialogTitle_default, cp as DialogDescription_default } from "./vendor-Cd6ZNPB1.js";
+const Dialog = __unplugin_components_1;
+Dialog.Title = DialogTitle_default;
+Dialog.Description = DialogDescription_default;
+export {
+  Dialog as D
+};
