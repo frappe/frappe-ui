d297b00f279312c5b33944bda90bbf09cf32687b
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-12-06 20:38:04 +0530

    feat: icon picker based on lucide icon sprite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
     './App.vue',
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './frappe/**/*.{vue,js,ts,jsx,tsx}',
+    './icons/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
