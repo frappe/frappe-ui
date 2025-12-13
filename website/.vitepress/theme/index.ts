92eb344ed2ad61ac5ed2b862387f5e34c2c42139
siduck <siduck@tutanota.com>
2025-12-13 15:05:34 +0530

    perf: eagerly load story components

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,20 +1,29 @@
-// https://vitepress.dev/guide/custom-theme
+// theme/index.ts
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
+// import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 import './style.css'
 import Demo from '../components/Demo.vue'
 
+const storyModules = import.meta.glob(
+  '../../../src/components/**/*.story.vue',
+  { eager: true },
+)
+
 export default {
-  // extends: DefaultTheme,
   Layout: () => {
-    return h(Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    return h(Layout, null, {})
   },
   enhanceApp({ app, router, siteData }) {
     app.component('ComponentPreview', Demo)
-    // ...
+
+    for (const path in storyModules) {
+      const match = path.match(/\/([^/]+)\.story\.vue$/)
+      if (match) {
+        const componentName = match[1]
+        app.component(componentName, (storyModules[path] as any).default)
+      }
+    }
   },
 } satisfies Theme
