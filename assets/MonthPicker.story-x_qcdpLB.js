91339a012bdb531a3d2ff04172b3c9a43d2b083b
surajshetty3416 <surajshetty3416@users.noreply.github.com>
2025-12-18 16:40:46 +0000

    deploy: 6ee0c356ca115870ce2c9ed72192bd66ea1480bc

---
+++ assets/MonthPicker.story-x_qcdpLB.js
@@ -0,0 +1,295 @@
+import { ay as markRaw, az as openBlock, aA as createElementBlock, aB as createBaseVNode, aC as defineComponent, aD as mergeModels, aN as ref, aE as useModel, aF as computed, aH as createBlock, aP as withCtx, aM as createVNode, aQ as createTextVNode, aL as toDisplayString, aK as createCommentVNode, aT as Fragment, aS as renderList, aO as resolveComponent } from "./vendor-DDsgpJno.js";
+import { B as Button } from "./Button-D69Os-R-.js";
+import { _ as __unplugin_components_1 } from "./Popover-Ci7RbmCM.js";
+import { L as LucideChevronLeft } from "./chevron-left-D8aPO9Hw.js";
+import { L as LucideChevronRight } from "./chevron-right-BlqGRNBf.js";
+import { _ as _export_sfc } from "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./FeatherIcon-DQToP4NW.js";
+const _hoisted_1$2 = {
+  class: "lucide lucide-calendar",
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "24",
+  height: "24",
+  viewBox: "0 0 24 24",
+  fill: "none",
+  stroke: "currentColor",
+  "stroke-width": "1.5",
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round"
+};
+function render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1$2, _cache[0] || (_cache[0] = [
+    createBaseVNode(
+      "path",
+      { d: "M8 2v4" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "M16 2v4" },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "rect",
+      {
+        width: "18",
+        height: "18",
+        x: "3",
+        y: "4",
+        rx: "2"
+      },
+      null,
+      -1
+      /* HOISTED */
+    ),
+    createBaseVNode(
+      "path",
+      { d: "M3 10h18" },
+      null,
+      -1
+      /* HOISTED */
+    )
+  ]));
+}
+const LucideCalender = markRaw({ name: "lucide-calendar", render });
+const _sfc_main$1 = /* @__PURE__ */ defineComponent({
+  __name: "MonthPicker",
+  props: /* @__PURE__ */ mergeModels({
+    placeholder: { type: String, required: false, default: "Select month" }
+  }, {
+    "modelValue": { type: null, ...{ default: "" } },
+    "modelModifiers": {}
+  }),
+  emits: ["update:modelValue"],
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const props = __props;
+    const viewMode = ref("month");
+    const model = useModel(__props, "modelValue");
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    const currentYear = ref((/* @__PURE__ */ new Date()).getFullYear());
+    const yearRangeStart = computed(
+      () => currentYear.value - currentYear.value % 12
+    );
+    const yearRange = computed(
+      () => Array.from({ length: 12 }, (_, i) => yearRangeStart.value + i)
+    );
+    const pickerList = computed(
+      () => viewMode.value == "year" ? yearRange.value : months
+    );
+    const toggleViewMode = () => {
+      viewMode.value = viewMode.value == "year" ? "month" : "year";
+    };
+    const handleOnClick = (v) => {
+      let ar = model.value.split(" ");
+      const indexToModify = viewMode.value == "year" ? 1 : 0;
+      ar[indexToModify] = String(v);
+      model.value = ar.join(" ");
+    };
+    const prevClick = () => {
+      currentYear.value += viewMode.value == "year" ? -12 : -1;
+    };
+    const nextClick = () => {
+      currentYear.value += viewMode.value == "year" ? 12 : 1;
+    };
+    const formatBtn = (v) => viewMode.value == "month" ? v.slice(0, 3) : v;
+    const __returned__ = { props, viewMode, model, months, currentYear, yearRangeStart, yearRange, pickerList, toggleViewMode, handleOnClick, prevClick, nextClick, formatBtn, Popover: __unplugin_components_1, get LucideCalender() {
+      return LucideCalender;
+    }, get LucideChevronLeft() {
+      return LucideChevronLeft;
+    }, get LucideChevronRight() {
+      return LucideChevronRight;
+    } };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1$1 = { class: "flex gap-2 justify-between" };
+const _hoisted_2$1 = { class: "grid grid-cols-3 gap-3" };
+function _sfc_render$1(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Button = Button;
+  return openBlock(), createBlock($setup["Popover"], { "popover-class": "mt-2 shadow-xl rounded-lg border bg-surface-modal p-2" }, {
+    target: withCtx(({ togglePopover }) => [
+      createVNode(_component_Button, {
+        onClick: togglePopover,
+        class: "w-full justify-between"
+      }, {
+        suffix: withCtx(() => [
+          createVNode($setup["LucideCalender"], { class: "size-4" })
+        ]),
+        default: withCtx(() => [
+          createTextVNode(
+            toDisplayString($setup.model || $setup.props.placeholder) + " ",
+            1
+            /* TEXT */
+          )
+        ]),
+        _: 2
+        /* DYNAMIC */
+      }, 1032, ["onClick"])
+    ]),
+    body: withCtx(() => [
+      createBaseVNode("div", _hoisted_1$1, [
+        createVNode(_component_Button, {
+          variant: "ghost",
+          onClick: $setup.prevClick
+        }, {
+          default: withCtx(() => [
+            createVNode($setup["LucideChevronLeft"], { class: "size-4 text-ink-gray-5" })
+          ]),
+          _: 1
+          /* STABLE */
+        }),
+        createCommentVNode(" view toggler "),
+        createVNode(_component_Button, { onClick: $setup.toggleViewMode }, {
+          default: withCtx(() => [
+            $setup.viewMode == "month" ? (openBlock(), createElementBlock(
+              Fragment,
+              { key: 0 },
+              [
+                createTextVNode(
+                  toDisplayString($setup.model.split(" ")[1] ?? $setup.currentYear),
+                  1
+                  /* TEXT */
+                )
+              ],
+              64
+              /* STABLE_FRAGMENT */
+            )) : (openBlock(), createElementBlock(
+              Fragment,
+              { key: 1 },
+              [
+                createTextVNode(
+                  toDisplayString($setup.yearRangeStart) + " - " + toDisplayString($setup.yearRangeStart + 11),
+                  1
+                  /* TEXT */
+                )
+              ],
+              64
+              /* STABLE_FRAGMENT */
+            ))
+          ]),
+          _: 1
+          /* STABLE */
+        }),
+        createVNode(_component_Button, {
+          variant: "ghost",
+          onClick: $setup.nextClick
+        }, {
+          default: withCtx(() => [
+            createVNode($setup["LucideChevronRight"], { class: "size-4 text-ink-gray-5" })
+          ]),
+          _: 1
+          /* STABLE */
+        })
+      ]),
+      _cache[0] || (_cache[0] = createBaseVNode(
+        "hr",
+        { class: "my-2" },
+        null,
+        -1
+        /* HOISTED */
+      )),
+      createCommentVNode(" picker btns  "),
+      createBaseVNode("div", _hoisted_2$1, [
+        (openBlock(true), createElementBlock(
+          Fragment,
+          null,
+          renderList($setup.pickerList, (x) => {
+            return openBlock(), createBlock(_component_Button, {
+              onClick: () => $setup.handleOnClick(x),
+              variant: $setup.model.includes(String(x)) ? "solid" : "ghost",
+              key: x,
+              class: "text-sm text-ink-gray-9"
+            }, {
+              default: withCtx(() => [
+                createTextVNode(
+                  toDisplayString($setup.formatBtn(x)),
+                  1
+                  /* TEXT */
+                )
+              ]),
+              _: 2
+              /* DYNAMIC */
+            }, 1032, ["onClick", "variant"]);
+          }),
+          128
+          /* KEYED_FRAGMENT */
+        ))
+      ])
+    ]),
+    _: 1
+    /* STABLE */
+  });
+}
+_sfc_main$1.__file = "src/components/MonthPicker/MonthPicker.vue";
+const MonthPicker = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["render", _sfc_render$1], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/MonthPicker/MonthPicker.vue"]]);
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "MonthPicker.story",
+  setup(__props, { expose: __expose }) {
+    __expose();
+    const val = ref("");
+    const __returned__ = { val, MonthPicker };
+    Object.defineProperty(__returned__, "__isScriptSetup", { enumerable: false, value: true });
+    return __returned__;
+  }
+});
+const _hoisted_1 = { class: "p-2" };
+const _hoisted_2 = { class: "p-2" };
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Variant = resolveComponent("Variant");
+  const _component_Story = resolveComponent("Story");
+  return openBlock(), createBlock(_component_Story, { layout: { type: "grid", width: 500 } }, {
+    default: withCtx(() => [
+      createVNode(_component_Variant, { title: "Default" }, {
+        default: withCtx(() => [
+          createBaseVNode("div", _hoisted_1, [
+            createVNode($setup["MonthPicker"], {
+              modelValue: $setup.val,
+              "onUpdate:modelValue": _cache[0] || (_cache[0] = ($event) => $setup.val = $event)
+            }, null, 8, ["modelValue"])
+          ])
+        ]),
+        _: 1
+        /* STABLE */
+      }),
+      createVNode(_component_Variant, { title: "Fit width" }, {
+        default: withCtx(() => [
+          createBaseVNode("div", _hoisted_2, [
+            createVNode($setup["MonthPicker"], {
+              modelValue: $setup.val,
+              "onUpdate:modelValue": _cache[1] || (_cache[1] = ($event) => $setup.val = $event),
+              class: "w-fit"
+            }, null, 8, ["modelValue"])
+          ])
+        ]),
+        _: 1
+        /* STABLE */
+      })
+    ]),
+    _: 1
+    /* STABLE */
+  });
+}
+_sfc_main.__file = "src/components/MonthPicker/MonthPicker.story.vue";
+const MonthPicker_story = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/home/runner/work/frappe-ui/frappe-ui/src/components/MonthPicker/MonthPicker.story.vue"]]);
+export {
+  MonthPicker_story as default
+};
