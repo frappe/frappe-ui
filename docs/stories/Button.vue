abc5a3196bb255219ecc13e402b5e6c23fc3faeb
siduck <siduck@tutanota.com>
2025-12-27 12:08:54 +0530

    feat(docs): include icons story in buttons page

--- docs/stories/Button.vue
+++ docs/stories/Button.vue
@@ -2,6 +2,9 @@
 import { reactive } from "vue";
 import { Button } from "frappe-ui";
 import Story from "@/components/Story.vue";
+import LucideBolt from "~icons/lucide/bolt";
+import LucideDownload from "~icons/lucide/download";
+import LucideArrowRight from "~icons/lucide/arrow-right";
 
 const state = reactive({
   theme: "gray",
@@ -51,6 +54,31 @@ const sizes = ["sm", "md", "lg", "xl", "2xl"];
       >
         {{ variant }}
       </Button>
+    </Story>
+
+    <Story title="With Icons" preview-css='flex gap-3'>
+      <Button>
+        <template #icon>
+          <LucideBolt class="size-4" />
+        </template>
+      </Button>
+
+      <Button>
+        <template #prefix>
+          <LucideDownload class="size-4" />
+        </template>
+
+				Download
+
+      </Button>
+
+			<Button>
+				<template #suffix>
+					<LucideArrowRight class="size-4" />
+				</template>
+
+				Get Started
+			</Button>
     </Story>
   </div>
 </template>
