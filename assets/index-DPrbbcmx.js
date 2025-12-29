9f7c1532891a39d88f07f4333a219386b6293758
nextchamp-saqib <nextchamp-saqib@users.noreply.github.com>
2025-12-29 06:47:18 +0000

    deploy: 168f7e926b82b40a67e9adec57d3e213603958b8

--- assets/index-DPrbbcmx.js
+++
@@ -1,8 +0,0 @@
-import { _ as __unplugin_components_1 } from "./Dialog-N0ZKhRng.js";
-import { cp as DialogTitle_default, cq as DialogDescription_default } from "./vendor-CiCJwPaP.js";
-const Dialog = __unplugin_components_1;
-Dialog.Title = DialogTitle_default;
-Dialog.Description = DialogDescription_default;
-export {
-  Dialog as D
-};
