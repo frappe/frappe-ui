8506074260e625acf17afb48a7019d1599f4f175
siduck <siduck@tutanota.com>
2025-12-29 16:22:37 +0530

    fix(select): typo in data-state attr selector for selected item bgcolor

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -124,7 +124,7 @@ defineSlots<{
             :key="option.value"
             :value="option.value"
             :class="[sizeClasses, paddingClasses, fontSizeClasses]"
-            class="text-base text-ink-gray-9 flex items-center data-[highlighted]:bg-surface-gray-2 border-0 [data-state=checked]:bg-surface-gray-2 data-[disabled]:text-ink-gray-4 select-none"
+            class="text-base text-ink-gray-9 flex items-center data-[highlighted]:bg-surface-gray-2 border-0 data-[state=checked]:bg-surface-gray-2 data-[disabled]:text-ink-gray-4 select-none"
           >
             <SelectItemText>
               <slot name="option" v-bind="{ option }">{{ option.label }}</slot>
