43c987cc24a72aaa568c8bdaf850edeca98f94dd
siduck <siduck@tutanota.com>
2025-12-29 15:48:46 +0530

    fix(docs): include missing icons in sidebar

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import icons from './theme/icons'
 
-const withIcon = (name: keyof typeof icons, text: string) => {
+const iconify = (name: keyof typeof icons, text: string) => {
   return `<span class='iconlink'>${icons[name]} ${text}</span>`
 }
 
@@ -24,7 +24,7 @@ function getComponentList() {
     .readdirSync(componentsPath, { withFileTypes: true })
     .filter((d) => d.isDirectory() && !excludeLinks.includes(d.name))
     .map((d) => ({
-      text: withIcon('box', d.name),
+      text: iconify('box', d.name),
       link: `/docs/${d.name.toLowerCase()}`,
     }))
 }
@@ -45,41 +45,41 @@ export default [
     collapsed: true,
     items: [
       {
-        text: withIcon('bgColor', 'Background Color'),
+        text: iconify('bgColor', 'Background Color'),
         link: '/docs/design-system/background-color',
       },
       {
-        text: withIcon('textColor', 'Text Color'),
+        text: iconify('textColor', 'Text Color'),
         link: '/docs/design-system/text-color',
       },
       {
-        text: withIcon('borderColor', 'Border Color'),
+        text: iconify('borderColor', 'Border Color'),
         link: '/docs/design-system/border-color',
       },
 
       {
-        text: withIcon('font', 'Font Size'),
+        text: iconify('font', 'Font Size'),
         link: '/docs/design-system/font-size',
       },
       {
-        text: withIcon('font', 'Font Weight'),
+        text: iconify('font', 'Font Weight'),
         link: '/docs/design-system/font-weight',
       },
       {
-        text: withIcon('font', 'Letter Spacing'),
+        text: iconify('font', 'Letter Spacing'),
         link: '/docs/design-system/letter-spacing',
       },
       {
-        text: withIcon('font', 'Line Height'),
+        text: iconify('font', 'Line Height'),
         link: '/docs/design-system/line-height',
       },
 
       {
-        text: withIcon('overlay', 'Drop Shadow'),
+        text: iconify('overlay', 'Drop Shadow'),
         link: '/docs/design-system/drop-shadow',
       },
       {
-        text: withIcon('radius', 'Border Radius'),
+        text: iconify('radius', 'Border Radius'),
         link: '/docs/design-system/border-radius',
       },
     ],
@@ -89,13 +89,13 @@ export default [
     text: 'Data Fetching',
     collapsed: false,
     items: [
-      { text: withIcon('db', 'Resource'), link: '/docs/resource' },
+      { text: iconify('db', 'Resource'), link: '/docs/resource' },
       {
-        text: withIcon('listdb', 'List Resource'),
+        text: iconify('listdb', 'List Resource'),
         link: '/docs/list-resource',
       },
       {
-        text: withIcon('docdb', 'Document Resource'),
+        text: iconify('docdb', 'Document Resource'),
         link: '/docs/document-resource',
       },
     ],
@@ -110,8 +110,8 @@ export default [
   {
     text: 'Other',
     items: [
-      { text: 'Utilities', link: '/docs/utilities' },
-      { text: 'directives', link: '/docs/directives' },
+      { text: iconify('cog', 'Utilities'), link: '/docs/utilities' },
+      { text: iconify('cog', 'directives'), link: '/docs/directives' },
     ],
   },
 ]
