0387a1a0f1b27141f242bc493d67d9edadf9c3f2
nextchamp-saqib <nextchamp-saqib@users.noreply.github.com>
2025-12-29 10:36:45 +0000

    deploy: 802367b3d11cc57e61c36452812c8c4a9ae538af

--- assets/index-BujQX5Of.js
+++
@@ -1,8 +0,0 @@
-import { _ as __unplugin_components_1 } from "./Dialog-8bYbVTnG.js";
-import { cs as DialogTitle_default, ct as DialogDescription_default } from "./vendor-BHA4RFAY.js";
-const Dialog = __unplugin_components_1;
-Dialog.Title = DialogTitle_default;
-Dialog.Description = DialogDescription_default;
-export {
-  Dialog as D
-};
