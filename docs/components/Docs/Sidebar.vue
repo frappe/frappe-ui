3f2d0332d2593184ac26d339ad20e6630cc29c41
siduck <siduck@tutanota.com>
2026-01-07 22:54:02 +0530

    fix(docs): only make the links div of sidebar scrollable

--- docs/components/Docs/Sidebar.vue
+++ docs/components/Docs/Sidebar.vue
@@ -16,10 +16,9 @@ const linkClass = "p-2 rounded";
   <aside
     class="
       scrollbar bg-surface-gray-1 dark:bg-surface-white z-50 border-r flex flex-col p-3 h-screen
-      overflow-hidden pr-0 pt-1 min-w-fit
-      hover:overflow-y-auto sticky top-0 w-full
+      pr-0 pt-1 min-w-fit
+      sticky top-0 w-full
     "
-		  style="scrollbar-gutter: stable;"
   >
     <a
       class="hidden lg:flex items-center gap-2 p-2 py-3 border-b mb-3 font-medium text-xl"
@@ -29,54 +28,59 @@ const linkClass = "p-2 rounded";
       Frappe UI
     </a>
 
-    <template v-for="(item, i) in sidebarConfig" :key="item.text">
-      <a
-        v-if="!item.items"
-        :href="item.link"
-        :class='
-          [
-            linkClass,
-            activeLink(item.link),
-            sidebarConfig[i + 1].items ? "mb-6" : "",
-          ]
-        '
-      >
-        {{ item.text }}
-      </a>
-
-      <details v-else class="group" :open="!item.collapsed">
-        <summary
-          class="
-            list-none w-full
-            flex items-center justify-between gap-2
-            whitespace-nowrap font-medium
-            text-ink-gray-600 border-t p-3 pr-1
-            group-open:text-ink-gray-800 cursor-pointer
-          "
+    <nav
+      class="flex flex-col overflow-hidden hover:overflow-y-auto"
+      style="scrollbar-gutter: stable"
+    >
+      <template v-for="(item, i) in sidebarConfig" :key="item.text">
+        <a
+          v-if="!item.items"
+          :href="item.link"
+          :class='
+            [
+              linkClass,
+              activeLink(item.link),
+              sidebarConfig[i + 1].items ? "mb-6" : "",
+            ]
+          '
         >
           {{ item.text }}
+        </a>
 
-          <ChevronRight
+        <details v-else class="group" :open="!item.collapsed">
+          <summary
             class="
-              size-4 shrink-0 ml-5
-              transition-transform duration-200
-              group-open:rotate-90
+              list-none w-full
+              flex items-center justify-between gap-2
+              whitespace-nowrap font-medium
+              text-ink-gray-600 border-t p-3 pr-1
+              group-open:text-ink-gray-800 cursor-pointer
             "
-          />
-        </summary>
-
-        <div class="flex flex-col mb-6">
-          <a
-            :href="child.link"
-            v-for="child in item.items"
-            :key="child.text"
-            class="inline-flex gap-2 items-center"
-            :class="[linkClass, activeLink(child.link)]"
           >
-            <component :is="child.icon" class="size-4" /> {{ child.text }}
-          </a>
-        </div>
-      </details>
-    </template>
+            {{ item.text }}
+
+            <ChevronRight
+              class="
+                size-4 shrink-0 ml-5
+                transition-transform duration-200
+                group-open:rotate-90
+              "
+            />
+          </summary>
+
+          <div class="flex flex-col mb-6">
+            <a
+              :href="child.link"
+              v-for="child in item.items"
+              :key="child.text"
+              class="inline-flex gap-2 items-center"
+              :class="[linkClass, activeLink(child.link)]"
+            >
+              <component :is="child.icon" class="size-4" /> {{ child.text }}
+            </a>
+          </div>
+        </details>
+      </template>
+    </nav>
   </aside>
 </template>
