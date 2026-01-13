bec756dff6e76a09b2e8221c228fd5a7b6bcaa64
siduck <siduck@tutanota.com>
2026-01-13 07:04:42 +0530

    chore(docs): remove useless code from coomponentPrevew.ts

--- docs/.vitepress/plugins/componentPreview.ts
+++ docs/.vitepress/plugins/componentPreview.ts
@@ -4,10 +4,10 @@ import { dirname, resolve } from 'node:path'
 export default function (md: MarkdownRenderer) {
   md.core.ruler.after('inline', 'component-preview', (state) => {
     const regex =
-      /<ComponentPreview\s+name=["']([^"']+)["'](?:\s+title=["']([^"']*)["'])?(?:\s+description=["']([^"']*)["'])?(?:\s+csr=["'](true|false)["'])?\s*\/>/g
+      /<ComponentPreview\s+name=["']([^"']+)["'](?:\s+csr=["'](true|false)["'])?\s*\/>/g
 
-    state.src = state.src.replace(regex, (_, name, title, description, csr) => {
-			const componentPath = `../../../../src/components/${name}/${name}.story.vue`
+    state.src = state.src.replace(regex, (_, name, csr) => {
+      const componentPath = `../../../../src/components/${name}/${name}.story.vue`
       const scriptIdx = state.tokens.findIndex(
         (i) => i.type === 'html_block' && /<script setup>/.test(i.content),
       )
@@ -25,17 +25,10 @@ export default function (md: MarkdownRenderer) {
 
       const idx = state.tokens.findIndex((i) => i.content.match(regex))
       const { realPath, path: _path } = state.env as MarkdownEnv
-      const props = [
-        name && `name="${name}"`,
-        title && `title="${title}"`,
-        description && `description="${description}"`,
-      ]
-        .filter(Boolean)
-        .join(' ')
 
       const open = csr ? '<ClientOnly>' : ''
       state.tokens[idx].content =
-        `${open}<ComponentPreview ${props}><Preview /><template #code>`
+        `${open}<ComponentPreview name="${name}"><Preview /><template #code>`
 
       const code = new state.Token('fence', 'code', 0)
       code.info = 'vue'
