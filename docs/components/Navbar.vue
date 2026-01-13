af67e84c387514295130dbca14f096c565614271
siduck <siduck@tutanota.com>
2026-01-13 13:09:08 +0530

    fix(docs): breadcrumbs remove 1st chevron

--- docs/components/Navbar.vue
+++ docs/components/Navbar.vue
@@ -1,92 +1,95 @@
 <script setup>
-import { computed, onMounted, ref, watch } from "vue";
-import { Button } from "frappe-ui";
-import LucideSun from "~icons/lucide/sun";
-import LucideMoon from "~icons/lucide/moon-star";
-import LucideSearch from "~icons/lucide/search";
-import LucideMenu from "~icons/lucide/menu";
-import LucideSide from "~icons/lucide/panel-right";
-import GithubIcon from "./Icons/Github.vue";
-import LucideRight from "~icons/lucide/chevron-right";
-import SearchPopup from "./Search/Popup.vue"
+import { computed, onMounted, ref, watch } from 'vue'
+import { Button } from 'frappe-ui'
+import LucideSun from '~icons/lucide/sun'
+import LucideMoon from '~icons/lucide/moon-star'
+import LucideSearch from '~icons/lucide/search'
+import LucideMenu from '~icons/lucide/menu'
+import LucideSide from '~icons/lucide/panel-right'
+import GithubIcon from './Icons/Github.vue'
+import LucideRight from '~icons/lucide/chevron-right'
+import SearchPopup from './Search/Popup.vue'
 
-import { state } from "../state";
+import { state } from '../state'
 import { useMagicKeys, whenever } from '@vueuse/core'
-import { useRoute } from "vitepress";
+import { useRoute } from 'vitepress'
 
-const theme = ref();
+const theme = ref()
 
 const toggleTheme = () => {
-  theme.value = theme.value === "dark" ? "light" : "dark";
-  document.documentElement.setAttribute("data-theme", theme.value);
-  localStorage.theme = theme.value;
-};
+  theme.value = theme.value === 'dark' ? 'light' : 'dark'
+  document.documentElement.setAttribute('data-theme', theme.value)
+  localStorage.theme = theme.value
+}
 
-const toggleMobSidebar = () => state.mobsidebar = !state.mobsidebar;
-const toggleNavbar = () => state.mobnavbar = !state.mobnavbar;
+const toggleMobSidebar = () => (state.mobsidebar = !state.mobsidebar)
+const toggleNavbar = () => (state.mobnavbar = !state.mobnavbar)
 
 onMounted(() => {
-  theme.value = document.documentElement.getAttribute("data-theme");
-});
+  theme.value = document.documentElement.getAttribute('data-theme')
+})
 
 defineProps({
   isDocs: {
     type: Boolean,
   },
-});
+})
 
-const route = useRoute();
+const route = useRoute()
 
 const routes = computed(() => {
-  let arr = route.path.split("/");
-  arr = arr.filter((x) => !["/", ""].includes(x));
+  let arr = route.path.split('/')
+  arr = arr.filter((x) => !['/', ''].includes(x))
 
   return arr.map((x, i) => {
-    let prevlinks = arr.slice(0, i + 1).join("/");
-    return { text: x, link: "/" + prevlinks };
-  });
-});
+    let prevlinks = arr.slice(0, i + 1).join('/')
+    return { text: x, link: '/' + prevlinks }
+  })
+})
 
 watch(route, (x) => {
-  state.mobnavbar = false;
-  state.mobsidebar = false;
-});
+  state.mobnavbar = false
+  state.mobsidebar = false
+})
 
 const { meta_k } = useMagicKeys()
 
 whenever(meta_k, (n) => {
   if (n) state.searchDialog = true
 })
-
 </script>
 
 <template>
   <nav class="border-b sticky top-0 bg-surface-white !z-50">
-    <SearchPopup v-if="state.searchDialog" @close="state.searchDialog = false" />
+    <SearchPopup
+      v-if="state.searchDialog"
+      @close="state.searchDialog = false"
+    />
 
     <div
       class="py-3 px-5 flex items-center gap-3 flex-wrap"
-      :class='{ "max-w-[1440px] mx-auto": !isDocs }'
+      :class="{ 'max-w-[1440px] mx-auto': !isDocs }"
     >
       <span
         class="flex gap-3 items-center mr-auto lg:mr-0"
-        :class='{ "lg:hidden": isDocs }'
+        :class="{ 'lg:hidden': isDocs }"
       >
         <img src="/logo.svg" class="w-5" />
         <a href="/" class="font-medium">Frappe UI</a>
       </span>
 
       <!-- minimal breadcrumbs -->
-      <div class="hidden lg:flex items-center gap-3">
-        <a
-          v-for="(x, i) in routes"
-          class="capitalize flex gap-2 items-center text-ink-gray-5"
-          :class='{ "text-ink-gray-9": i === routes.length - 1 }'
-          :href="x.link"
-        >
-          <LucideRight class="size-4" />
-          {{ x.text }}
-        </a>
+      <div class="hidden lg:flex items-center gap-1">
+        <template v-for="(x, i) in routes">
+          <LucideRight class="size-4 text-ink-gray-5" v-if="i != 0" />
+          <a
+            class="capitalize flex gap-1 items-center text-ink-gray-5 hover:text-ink-gray-9"
+            :class="{ 'text-ink-gray-9': i === routes.length - 1 }"
+            :href="x.link"
+          >
+            {{ x.text }}
+          </a>
+        </template>
       </div>
 
       <Button v-if="isDocs" @click="toggleMobSidebar" class="lg:hidden">
@@ -103,7 +106,7 @@ whenever(meta_k, (n) => {
 
       <div
         class="gap-3 lg:flex items-center w-full ml-auto lg:w-auto"
-        :class='{ "flex": state.mobnavbar, "hidden": !state.mobnavbar }'
+        :class="{ flex: state.mobnavbar, hidden: !state.mobnavbar }"
       >
         <a href="/docs">Docs</a>
 
@@ -116,7 +119,7 @@ whenever(meta_k, (n) => {
 
         <Button @click="toggleTheme" class="rounded">
           <template #icon>
-            <LucideSun v-if='theme === "dark"' class="size-4" />
+            <LucideSun v-if="theme === 'dark'" class="size-4" />
             <LucideMoon v-else class="size-4" />
           </template>
         </Button>
