658b64d3e68586bc8d7cf970f51dfe31bb623044
netchampfaris <netchampfaris@users.noreply.github.com>
2026-01-16 11:26:57 +0000

    deploy: 19669912e0fb1342ee3f1397f82440138459e8f0

---
+++ assets/index-B1ERwyAP.js
@@ -0,0 +1,8 @@
+import { _ as __unplugin_components_1 } from "./Dialog-GFgPWtJs.js";
+import { cs as DialogTitle_default, ct as DialogDescription_default } from "./vendor-Dk9TTFVx.js";
+const Dialog = __unplugin_components_1;
+Dialog.Title = DialogTitle_default;
+Dialog.Description = DialogDescription_default;
+export {
+  Dialog as D
+};
