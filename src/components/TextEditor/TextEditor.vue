4dbd097569a77a7e74c0fa8d6ba11cd3f16c8290
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-01-21 15:06:10 +0530

    fix: spacing issues

--- src/components/TextEditor/TextEditor.vue
+++ src/components/TextEditor/TextEditor.vue
@@ -1,38 +1,38 @@
 <template>
-  <div
-    v-if="editor"
-    class="relative w-full"
-    :class="attrsClass"
-    :style="attrsStyle"
-    v-bind="attrsWithoutClassStyle"
-    ref="rootRef"
-  >
-    <TextEditorBubbleMenu :buttons="bubbleMenu" :options="bubbleMenuOptions" />
-    <TextEditorFixedMenu
-      class="w-full overflow-x-auto rounded-t-lg border border-outline-gray-modals"
-      :buttons="fixedMenu"
-    />
-    <TextEditorFloatingMenu :buttons="floatingMenu" />
-    <slot name="top" :editor />
-    <slot name="editor" :editor="editor">
-      <EditorContent :editor="editor" />
-    </slot>
-    <slot name="bottom" :editor />
-  </div>
+	<div
+		v-if="editor"
+		class="relative w-full"
+		:class="attrsClass"
+		:style="attrsStyle"
+		v-bind="attrsWithoutClassStyle"
+		ref="rootRef"
+	>
+		<TextEditorBubbleMenu :buttons="bubbleMenu" :options="bubbleMenuOptions" />
+		<TextEditorFixedMenu
+			class="w-full overflow-x-auto rounded-t-lg border border-outline-gray-modals"
+			:buttons="fixedMenu"
+		/>
+		<TextEditorFloatingMenu :buttons="floatingMenu" />
+		<slot name="top" :editor />
+		<slot name="editor" :editor="editor">
+			<EditorContent :editor="editor" />
+		</slot>
+		<slot name="bottom" :editor />
+	</div>
 </template>
 
 <script setup lang="ts">
 import {
-  normalizeClass,
-  normalizeStyle,
-  computed,
-  watch,
-  onMounted,
-  onBeforeUnmount,
-  provide,
-  ref,
-  useAttrs,
-  useTemplateRef,
+	normalizeClass,
+	normalizeStyle,
+	computed,
+	watch,
+	onMounted,
+	onBeforeUnmount,
+	provide,
+	ref,
+	useAttrs,
+	useTemplateRef,
 } from 'vue'
 
 defineOptions({ inheritAttrs: false })
@@ -73,9 +73,9 @@ import { TextEditorEmits, TextEditorProps } from './types'
 import { getTagExtensions } from './extensions/tag'
 
 function defaultUploadFunction(file: File) {
-  // useFileUpload is frappe specific
-  let fileUpload = useFileUpload()
-  return fileUpload.upload(file, props.uploadArgs || {})
+	// useFileUpload is frappe specific
+	let fileUpload = useFileUpload()
+	return fileUpload.upload(file, props.uploadArgs || {})
 }
 
 const props = withDefaults(defineProps<TextEditorProps>(), {
@@ -102,53 +102,53 @@ const attrs = useAttrs()
 const attrsClass = computed(() => normalizeClass(attrs.class))
 const attrsStyle = computed(() => normalizeStyle(attrs.style))
 const attrsWithoutClassStyle = computed(() => {
-  return Object.fromEntries(
-    Object.entries(attrs).filter(([key]) => key !== 'class' && key !== 'style'),
-  )
+	return Object.fromEntries(
+		Object.entries(attrs).filter(([key]) => key !== 'class' && key !== 'style'),
+	)
 })
 
 const editorProps = computed(() => {
-  return {
-    attributes: {
-      class: normalizeClass([
-        'prose prose-table:table-fixed prose-td:p-2 prose-th:p-2 prose-td:border prose-th:border prose-td:border-outline-gray-2 prose-th:border-outline-gray-2 prose-td:relative prose-th:relative prose-th:bg-surface-gray-2',
-        props.editorClass,
-      ]),
-    },
-  }
+	return {
+		attributes: {
+			class: normalizeClass([
+				'prose prose-table:table-fixed prose-td:p-2 prose-th:p-2 prose-td:border prose-th:border prose-td:border-outline-gray-2 prose-th:border-outline-gray-2 prose-td:relative prose-th:relative prose-th:bg-surface-gray-2',
+				props.editorClass,
+			]),
+		},
+	}
 })
 
 watch(
-  () => props.content,
-  (val) => {
-    if (editor.value) {
-      let currentHTML = editor.value.getHTML()
-      if (currentHTML !== val) {
-        editor.value.commands.setContent(val)
-      }
-    }
-  },
+	() => props.content,
+	(val) => {
+		if (editor.value) {
+			let currentHTML = editor.value.getHTML()
+			if (currentHTML !== val) {
+				editor.value.commands.setContent(val)
+			}
+		}
+	},
 )
 
 watch(
-  () => props.editable,
-  (value) => {
-    if (editor.value) {
-      editor.value.setEditable(value)
-    }
-  },
+	() => props.editable,
+	(value) => {
+		if (editor.value) {
+			editor.value.setEditable(value)
+		}
+	},
 )
 
 watch(
-  editorProps,
-  (value) => {
-    if (editor.value) {
-      editor.value.setOptions({
-        editorProps: value,
-      })
-    }
-  },
-  { deep: true },
+	editorProps,
+	(value) => {
+		if (editor.value) {
+			editor.value.setOptions({
+				editorProps: value,
+			})
+		}
+	},
+	{ deep: true },
 )
 
 onMounted(() => {
@@ -249,21 +249,21 @@ onMounted(() => {
 })
 
 onBeforeUnmount(() => {
-  if (editor.value) {
-    editor.value.destroy()
-    editor.value = null
-  }
+	if (editor.value) {
+		editor.value.destroy()
+		editor.value = null
+	}
 })
 
 provide(
-  'editor',
-  computed(() => editor.value),
+	'editor',
+	computed(() => editor.value),
 )
 
 const rootRef = useTemplateRef('rootRef')
 defineExpose({
-  editor,
-  rootRef,
+	editor,
+	rootRef,
 })
 </script>
 
@@ -272,130 +272,138 @@ defineExpose({
 @import './extensions/highlight/highlight-styles.css';
 
 .ProseMirror {
-  outline: none;
-  caret-color: var(--ink-gray-9);
-  word-break: break-word;
+	outline: none;
+	caret-color: var(--ink-gray-9);
+	word-break: break-word;
 }
 
 /* Firefox */
 .ProseMirror-focused:focus-visible {
-  outline: none;
+	outline: none;
 }
 
 /* Placeholder */
 .ProseMirror:not(.ProseMirror-focused) p.is-editor-empty::before {
-  content: attr(data-placeholder);
-  float: left;
-  color: var(--ink-gray-4);
-  pointer-events: none;
-  height: 0;
+	content: attr(data-placeholder);
+	float: left;
+	color: var(--ink-gray-4);
+	pointer-events: none;
+	height: 0;
 }
 
 .ProseMirror-selectednode video,
 img.ProseMirror-selectednode {
-  outline: 2px solid var(--outline-gray-2);
+	outline: 2px solid var(--outline-gray-2);
 }
 
 /* Table styles */
 .prose table p {
-  margin: 0;
+	margin: 0;
 }
 
 /* Prosemirror specific table styles */
 .ProseMirror table .selectedCell:after {
-  z-index: 2;
-  position: absolute;
-  content: '';
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  pointer-events: none;
-  background: theme('colors.blue.200');
-  opacity: 0.3;
+	z-index: 2;
+	position: absolute;
+	content: '';
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	pointer-events: none;
+	background: theme('colors.blue.200');
+	opacity: 0.3;
 }
 
 .ProseMirror table .column-resize-handle {
-  position: absolute;
-  right: -1px;
-  top: 0;
-  bottom: -2px;
-  width: 4px;
-  background-color: theme('colors.blue.200');
-  pointer-events: none;
+	position: absolute;
+	right: -1px;
+	top: 0;
+	bottom: -2px;
+	width: 4px;
+	background-color: theme('colors.blue.200');
+	pointer-events: none;
 }
 
 .ProseMirror ul[data-type='taskList'] {
-  list-style: none;
-  padding: 0;
+	list-style: none;
+	padding: 0;
 
-  li {
-    align-items: flex-start;
-    display: flex;
-    margin: 0;
+	li {
+		align-items: flex-start;
+		display: flex;
+		margin: 0;
 
-    > label {
-      flex: 0 0 auto;
-      margin-right: 0.5rem;
-      margin-top: 0.25rem;
-      height: 1lh;
-      display: flex;
-      align-items: center;
-      user-select: none;
-    }
+		> label {
+			flex: 0 0 auto;
+			margin-right: 0.5rem;
+			margin-top: 0.25rem;
+			height: 1lh;
+			display: flex;
+			align-items: center;
+			user-select: none;
+		}
 
-    > div {
-      flex: 1 1 auto;
-      margin-bottom: 0;
+		> div {
+			flex: 1 1 auto;
+			margin-bottom: 0;
 
-      > p {
-        margin: 0.25rem 0;
-      }
-    }
-  }
-  ul[data-type='taskList'] {
-    margin: 0;
-  }
+			> p {
+				margin: 0.25rem 0;
+			}
+		}
+	}
+	ul[data-type='taskList'] {
+		margin: 0;
+	}
 
-  input[type='checkbox'] {
-    cursor: pointer;
-    width: 14px;
-    height: 14px;
-    border-radius: 4px;
-    color: theme('colors.gray.900');
-  }
+	input[type='checkbox'] {
+		cursor: pointer;
+		width: 14px;
+		height: 14px;
+		border-radius: 4px;
+		color: theme('colors.gray.900');
+	}
 }
 
 .resize-cursor {
-  cursor: ew-resize;
-  cursor: col-resize;
+	cursor: ew-resize;
+	cursor: col-resize;
 }
 
 .tag-item,
 .tag-suggestion-active {
-  background-color: var(--surface-gray-1, #f8f8f8);
-  color: inherit;
-  border: 1px solid transparent;
-  padding: 0px 2px;
-  border-radius: 4px;
-  font-size: 1em;
-  white-space: nowrap;
-  cursor: default;
+	background-color: var(--surface-gray-1, #f8f8f8);
+	color: inherit;
+	border: 1px solid transparent;
+	padding: 0px 2px;
+	border-radius: 4px;
+	font-size: 1em;
+	white-space: nowrap;
+	cursor: default;
 }
 
 .tag-item.ProseMirror-selectednode {
-  border-color: var(--outline-gray-3, #c7c7c7);
+	border-color: var(--outline-gray-3, #c7c7c7);
 }
 
 .tag-suggestion-active {
-  background-color: var(--surface-gray-2, #f3f3f3);
+	background-color: var(--surface-gray-2, #f3f3f3);
 }
 
 /* Edit prose classes to be more functional */
 .prose-sm {
-  blockquote {
-    quotes: none;
-    font-style: normal;
-  }
+	blockquote {
+		quotes: none;
+		font-style: normal;
+	}
+	/* fix: heading bottom margin */
+	:is(h1, h2, h3, h4, h5, h6) + :is(h1, h2, h3, h4, h5, h6) {
+		margin-top: 0;
+	}
+
+	hr {
+		margin: 2.25em 0;
+	}
 }
 </style>
