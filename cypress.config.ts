34acbb2ab25400a4d489e33868422def381ea0da
siduck <siduck@tutanota.com>
2026-03-18 14:11:09 +0530

    feat(tests): use cypress-split for parallel tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
+import cypressSplit from 'cypress-split'
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "vue",
-      bundler: "vite",
+      framework: 'vue',
+      bundler: 'vite',
+    },
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
     },
   },
-});
+})
