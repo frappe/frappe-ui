ee5af38f2ab79ab4ba0262c2180f9d02de534e6f
Faris Ansari <netchamp.faris@gmail.com>
2026-04-22 17:57:42 +0530

    docs: use prose-v3 style

--- docs/components/Layout.vue
+++ docs/components/Layout.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import Navbar from "./Navbar.vue";
-import Sidebar from "./Docs/Sidebar.vue";
-import Home from "./Home/index.vue";
+import Navbar from './Navbar.vue'
+import Sidebar from './Docs/Sidebar.vue'
+import Home from './Home/index.vue'
 
-import OnThisPage from "./Docs/OnThisPage.vue";
-import PrevNextBtns from "./Docs/PrevNextBtns.vue";
+import OnThisPage from './Docs/OnThisPage.vue'
+import PrevNextBtns from './Docs/PrevNextBtns.vue'
 
-import { state } from "../state.ts";
+import { state } from '../state.ts'
 
-import { useData } from "vitepress";
-const { frontmatter } = useData();
+import { useData } from 'vitepress'
+const { frontmatter } = useData()
 </script>
 
 <template>
   <div
-    v-if='frontmatter.layout === "home"'
+    v-if="frontmatter.layout === 'home'"
     class="h-full flex flex-col justify-between flex-1 w-full"
   >
     <Navbar />
@@ -31,7 +31,10 @@ const { frontmatter } = useData();
 
         <div class="p-5 lg:p-10 flex gap-5">
           <main class="mx-auto lg:max-w-[740px] flex-1">
-            <Content as="article" class="prose prose-sm !max-w-none" />
+            <Content
+              as="article"
+              class="prose prose-v3 prose-p:mb-4 !max-w-none"
+            />
             <PrevNextBtns />
           </main>
           <OnThisPage />
