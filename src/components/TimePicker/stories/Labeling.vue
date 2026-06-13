ef8af40160c902e8be054f60d817be026c3dd3a8
shariquerik <sharique.rik@gmail.com>
2026-06-13 14:09:05 +0530

    docs: add labeling and sizes/variants stories to TimePicker

---
+++ src/components/TimePicker/stories/Labeling.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { TimePicker } from 'frappe-ui'
+</script>
+
+<template>
+  <div class="flex w-72 flex-col gap-4 items-start">
+    <TimePicker label="Default" model-value="14:30" />
+    <TimePicker
+      label="With description"
+      description="Pick when the meeting should start."
+      model-value="14:30"
+    />
+    <TimePicker label="Required" required model-value="14:30" />
+    <TimePicker label="Disabled" disabled model-value="14:30" />
+    <TimePicker
+      label="With error"
+      error="Please select a time."
+      model-value="14:30"
+    />
+  </div>
+</template>
