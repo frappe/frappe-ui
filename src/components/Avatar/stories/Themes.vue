2aa7f4a226481fed307352afa1d2da39acb45ae9
Bread Genie <msuhailbh07@gmail.com>
2026-07-06 18:05:12 +0530

    fix: add missing file

---
+++ src/components/Avatar/stories/Themes.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Avatar } from 'frappe-ui'
+import type { AvatarTheme } from 'frappe-ui'
+
+const themes: AvatarTheme[] = [
+  'gray',
+  'blue',
+  'green',
+  'amber',
+  'red',
+  'violet',
+  'orange',
+  'auto',
+]
+</script>
+
+<template>
+  <Avatar
+    v-for="theme in themes"
+    :key="theme"
+    :label="theme === 'auto' ? 'Jane Doe' : theme"
+    :theme="theme"
+    size="lg"
+  />
+</template>
